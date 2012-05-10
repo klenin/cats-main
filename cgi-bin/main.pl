3698ed7a4547e2c7403b58ec0bfc33d1825a8244
Dmitry Kravtsov <idkravitz@gmail.com>
2012-05-10 21:00:04 +1100

    Convert main_envelope.htm for TT

--- cgi-bin/main.pl
+++ cgi-bin/main.pl
@@ -2559,7 +2559,7 @@ sub rank_problem_details
 
 sub envelope_frame
 {
-    init_template('main_envelope.htm');
+    init_template('envelope.html.tt');
     
     my $rid = url_param('rid') or return;
 
