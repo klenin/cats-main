7a960bb89cef21192cf74a3cfe74b19b78978bd1
Alexander Klenin <klenin@gmail.com>
2014-09-16 13:23:07 +1100

    CATS::Data: Translate comments. Whitespace fixes.

--- cgi-bin/CATS/Data.pm
+++ cgi-bin/CATS/Data.pm
@@ -23,38 +23,33 @@ BEGIN
     our %EXPORT_TAGS = (all => [ @EXPORT ]);
 }
 
-
-# Получить информацию об участнике турнира.
-# параметры: fields, contest_id, account_id.
+# Params: fields, contest_id, account_id.
 sub get_registered_contestant
 {
     my %p = @_;
     $p{fields} ||= 1;
     $p{account_id} ||= $uid or return;
     $p{contest_id} or die;
-    
+
     $dbh->selectrow_array(qq~
         SELECT $p{fields} FROM contest_accounts WHERE contest_id = ? AND account_id = ?~, undef,
         $p{contest_id}, $p{account_id});
 }
 
-
 sub is_jury_in_contest
 {
     my %p = @_;
     return 1 if $is_root;
-    # Оптимизация: если запрос о текущем турнире, вывести уже считанное значение.
-    if (defined $cid && $p{contest_id} == $cid)
-    {
+    # Optimization: if the request is about the current contest, return cached value.
+    if (defined $cid && $p{contest_id} == $cid) {
         return $is_jury;
     }
     my ($j) = get_registered_contestant(fields => 'is_jury', @_);
     return $j;
 }
 
-
-# Вручную прописать результат попытки участника. Также можно применять для повторного тестирования.
-# Параметры: request_id, state, failed_test, testsets.
+# Set request state manually. May be also used for retesting.
+# Params: request_id, state, failed_test, testsets.
 sub enforce_request_state
 {
     my %p = @_;
@@ -66,14 +61,13 @@ sub enforce_request_state
             WHERE id = ?~, {},
         $p{failed_test}, $p{state}, $p{testsets}, $p{request_id}
     ) or return;
-    if ($p{state} != $cats::st_ignore_submit) # сохраняем лог игнорируемых попыток
-    {
+    # Save log for ignored requests.
+    if ($p{state} != $cats::st_ignore_submit) {
         $dbh->do(qq~DELETE FROM log_dumps WHERE req_id = ?~, {}, $p{request_id})
             or return;
     }
     $dbh->commit;
     return 1;
 }
-
 
 1;
