125af1c0127499b77cfc4f77cd5373c44214b405
Alexander Klenin <klenin@gmail.com>
2015-02-14 17:02:44 +1000

    Extract CATS::UI::LoginLogout

--- cgi-bin/main.pm
+++ cgi-bin/main.pm
@@ -57,76 +57,7 @@ use CATS::UI::Problems;
 use CATS::UI::Contests;
 use CATS::UI::Users;
 use CATS::UI::ImportSources;
-
-sub make_sid {
-    my @ch = ('A'..'Z', 'a'..'z', '0'..'9');
-    join '', map { $ch[rand @ch] } 1..30;
-}
-
-
-sub login_frame
-{
-    my $json = param('json');
-    init_template(auto_ext('login', $json));
-    $t->param(href_login => url_function('login'));
-    msg(1004) if param('logout');
-
-    my $login = param('login');
-    if (!$login) {
-        $t->param(message => 'No login') if $json;
-        return;
-    }
-    $t->param(login => Encode::decode_utf8($login));
-    my $passwd = param('passwd');
-
-    my ($aid, $passwd2, $locked) = $dbh->selectrow_array(qq~
-        SELECT id, passwd, locked FROM accounts WHERE login = ?~, undef, $login);
-
-    $aid && $passwd2 eq $passwd or return msg(1040);
-    !$locked or return msg(1041);
-
-    my $last_ip = CATS::IP::get_ip();
-
-    my $cid = url_param('cid');
-    for (1..20) {
-        $sid = make_sid;
-
-        $dbh->do(qq~
-            UPDATE accounts SET sid = ?, last_login = CURRENT_TIMESTAMP, last_ip = ?
-                WHERE id = ?~,
-            {}, $sid, $last_ip, $aid
-        ) or next;
-        $dbh->commit;
-
-        if ($json) {
-            $contest->load($cid, ['id']);
-            $t->param(sid => $sid, cid => $contest->{id});
-            return;
-        }
-        $t = undef;
-        return redirect(url_function('contests', sid => $sid, cid => $cid));
-    }
-    die 'Can not generate sid';
-}
-
-
-sub logout_frame
-{
-    $cid = '';
-    $sid = '';
-    if ($uid) {
-        $dbh->do(qq~UPDATE accounts SET sid = NULL WHERE id = ?~, {}, $uid);
-        $dbh->commit;
-    }
-    if (param('json')) {
-        init_template(auto_ext('logout'));
-        0;
-    }
-    else {
-       redirect(url_function('login', logout => 1));
-    }
-}
-
+use CATS::UI::LoginLogout;
 
 sub prizes_frame
 {
@@ -312,8 +243,8 @@ sub generate_menu
 sub interface_functions ()
 {
     {
-        login => \&login_frame,
-        logout => \&logout_frame,
+        login => \&CATS::UI::LoginLogout::login_frame,
+        logout => \&CATS::UI::LoginLogout::logout_frame,
         registration => \&CATS::UI::Users::registration_frame,
         settings => \&CATS::UI::Users::settings_frame,
         contests => \&CATS::UI::Contests::contests_frame,
@@ -352,7 +283,6 @@ sub interface_functions ()
         problem_text => \&CATS::Problem::Text::problem_text_frame,
         envelope => \&CATS::UI::Messages::envelope_frame,
         about => \&about_frame,
-        static => \&static_frame,
 
         similarity => \&CATS::UI::Stats::similarity_frame,
         personal_official_results => \&CATS::Contest::personal_official_results,
