klenin/cats-main

commit c94ad5e855eee1dfcc54f0e4c436041d66085272

problem: add 'run method' propery

cgi-bin/CATS/Problem.pm
@@ -24,7 +24,7 @@ use fields qw(
     tests testsets samples objects keywords
     imports solutions generators modules pictures attachments
     test_defaults current_tests current_sample gen_groups
-    encoding stml zip zip_archive old_title replace tag_stack has_checker de_list
+    encoding stml zip zip_archive old_title replace tag_stack has_checker de_list run_method
 );
 
 use CATS::Problem::Tests;
@@ -227,6 +227,8 @@ sub validate
         if @without_points && @without_points != @t;
 
     $check_order->($self->{samples}, 'sample');
+
+    $self->{run_method} ||= $cats::rm_default;
 }
 
 
@@ -374,6 +376,7 @@ sub tag_handlers()
     SampleOut => { s => \&start_tag_SampleOut, e => \&end_stml, in => ['Sample'] },
     Keyword => { s => \&start_tag_Keyword, r => ['code'] },
     Testset => { s => \&start_tag_Testset, r => ['name', 'tests'] },
+    Run => { s => \&start_tag_Run, r => ['method'] },
 }}
 
 
@@ -452,6 +455,7 @@ sub problem_source_common_params
         memory_limit => $atts->{memoryLimit},
     );
 }
+
 
 sub start_tag_Solution
 {
@@ -587,6 +591,7 @@ sub import_one_source
 
 }
 
+
 sub start_tag_Import
 {
     (my CATS::Problem $self, my $atts) = @_;
@@ -648,6 +653,7 @@ sub sample_in_out
     }
 }
 
+
 sub start_tag_SampleIn { $_[0]->sample_in_out($_[1], 'in_file'); }
 sub start_tag_SampleOut { $_[0]->sample_in_out($_[1], 'out_file'); }
 
@@ -673,6 +679,20 @@ sub start_tag_Testset
         id => new_id, map { $_ => $atts->{$_} } qw(name tests points comment hideDetails) };
     $self->{testsets}->{$n}->{hideDetails} ||= 0;
     $self->note("Testset $n added");
+}
+
+
+sub start_tag_Run
+{
+    (my CATS::Problem $self, my $atts) = @_;
+    $self->error("Duplicate 'Run' tag") if defined $self->{run_method};
+    my $m = $atts->{method};
+    $self->{run_method} = {
+        default => $cats::rm_default,
+        interactive => $cats::rm_interactive,
+    }->{$m};
+    $self->error("Unknown run method: '$m'") if not defined $self->{run_method};
+    $self->note("Run method set to '$m'");
 }
 
 
@@ -768,7 +788,7 @@ sub end_tag_Problem
             title=?, lang=?, time_limit=?, memory_limit=?, difficulty=?, author=?, input_file=?, output_file=?,
             statement=?, pconstraints=?, input_format=?, output_format=?, formal_input=?, json_data=?, explanation=?, zip_archive=?,
             upload_date=CURRENT_TIMESTAMP, std_checker=?, last_modified_by=?,
-            max_points=?, hash=NULL
+            max_points=?, hash=NULL, run_method=?
         WHERE id = ?~
     : q~
         INSERT INTO problems (
@@ -776,9 +796,9 @@ sub end_tag_Problem
             title, lang, time_limit, memory_limit, difficulty, author, input_file, output_file,
             statement, pconstraints, input_format, output_format, formal_input, json_data, explanation, zip_archive,
             upload_date, std_checker, last_modified_by,
-            max_points, id
+            max_points, id, run_method
         ) VALUES (
-            ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP,?,?,?,?
+            ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP,?,?,?,?,?
         )~;
 
     my $c = $dbh->prepare($sql);
@@ -792,6 +812,7 @@ sub end_tag_Problem
     $c->bind_param($i++, $CATS::Misc::uid);
     $c->bind_param($i++, $self->{problem}->{max_points});
     $c->bind_param($i++, $self->{id});
+    $c->bind_param($i++, $self->{run_method});
     $c->execute;
 
     $self->insert_problem_content;

cgi-bin/CATS/Constants.pm
@@ -76,6 +76,10 @@ $problem_st_suspended = 1;
 $problem_st_disabled  = 2;
 $problem_st_hidden    = 3;
 
+# Values for problems.run_method.
+$rm_default = 0;
+$rm_interactive = 1;
+
 $penalty = 20;
 
 @problem_codes = ('A'..'Z', '1'..'9');

sql/interbase/init_relations.sql
@@ -120,7 +120,8 @@ CREATE TABLE problems (
     zip_archive     BLOB,
     last_modified_by INTEGER REFERENCES accounts(id) ON DELETE SET NULL ON UPDATE CASCADE,
     max_points      INTEGER,
-    hash            VARCHAR(200)
+    hash            VARCHAR(200),
+    run_method      SMALLINT DEFAULT 0 CHECK (run_method IN (0, 1))
 );
 
 

commit 887f3fcb86409c280cd5628270f0e48f474e68ed

Problem: Fix problem uploading
Broken by c94ad5e855eee1dfcc54f0e4c436041d66085272

cgi-bin/CATS/Problem.pm
@@ -794,7 +794,7 @@ sub end_tag_Problem
             title=?, lang=?, time_limit=?, memory_limit=?, difficulty=?, author=?, input_file=?, output_file=?,
             statement=?, pconstraints=?, input_format=?, output_format=?, formal_input=?, json_data=?, explanation=?, zip_archive=?,
             upload_date=CURRENT_TIMESTAMP, std_checker=?, last_modified_by=?,
-            max_points=?, hash=NULL, run_method=?
+            max_points=?, run_method=?, hash=NULL
         WHERE id = ?~
     : q~
         INSERT INTO problems (
@@ -802,7 +802,7 @@ sub end_tag_Problem
             title, lang, time_limit, memory_limit, difficulty, author, input_file, output_file,
             statement, pconstraints, input_format, output_format, formal_input, json_data, explanation, zip_archive,
             upload_date, std_checker, last_modified_by,
-            max_points, id, run_method
+            max_points, run_method, id
         ) VALUES (
             ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP,?,?,?,?,?
         )~;
@@ -817,8 +817,8 @@ sub end_tag_Problem
     $c->bind_param($i++, $self->{problem}->{std_checker});
     $c->bind_param($i++, $CATS::Misc::uid);
     $c->bind_param($i++, $self->{problem}->{max_points});
-    $c->bind_param($i++, $self->{id});
     $c->bind_param($i++, $self->{run_method});
+    $c->bind_param($i++, $self->{id});
     $c->execute;
 
     $self->insert_problem_content;