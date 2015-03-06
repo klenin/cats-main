klenin/cats-main

commit 56a2fdd648b305d61c1e864961697f0b2c1ab669

Add git author's name and email to users
Only root users can change git data

cgi-bin/CATS/User.pm
@@ -90,7 +90,7 @@ sub new_frame
 
 sub param_names ()
 {
-    qw(login team_name capitan_name email country motto home_page icq_number city)
+    qw(login team_name capitan_name email country motto home_page icq_number city git_author_name git_author_email)
 }
 
 

cgi-bin/CATS/UI/Users.pm
@@ -129,6 +129,7 @@ sub settings_frame
         countries => \@CATS::Countries::countries, href_action => url_f('users'),
         title_suffix => res_str(518),
         langs => [ map { href => url_f('settings', lang => $_), name => $_ }, @cats::langs ],
+        is_root => $is_root,
         %$u);
     if ($is_root) {
         # Data::Dumper escapes UTF-8 characters into \x{...} sequences.

sql/interbase/init_relations.sql
@@ -33,9 +33,11 @@ CREATE TABLE accounts (
     locked      INTEGER DEFAULT 0 CHECK (locked IN (0, 1)),
     team_name   VARCHAR(200),
     capitan_name VARCHAR(200),
+    git_author_name VARCHAR(200) DEFAULT NULL,
     country     VARCHAR(30),
     motto       VARCHAR(200),
     email       VARCHAR(200),
+    git_author_email VARCHAR(200) DEFAULT NULL,
     home_page   VARCHAR(200),
     icq_number  VARCHAR(200),
     settings    BLOB SUB_TYPE 0,

tt/includes/user_extra.tti
@@ -11,5 +11,11 @@
   <tr><td>[% capt.$field %]</td>
   <td><input type="text" name="[% field %]" size="50" value="[% $field | html %]" class="bordered"></td></tr>
 [%- END -%]
+
+[% IF is_root %]
+  [% input_tr('git_author_name') %]
+  [% input_tr('git_author_email') %]
+[% END %]
+
 [% FOREACH f IN ['city', 'motto', 'capitan_name', 'email', 'icq_number', 'home_page']; input_tr(f); END %]
 </table>

tt/lang/en/user_extra.tti
@@ -7,5 +7,7 @@
     email => 'E-Mail',
     icq_number => 'ICQ',
     home_page => 'Home page',
+    git_author_name => "Git author's name",
+    git_author_email => "Git author's e-mail",
   })
 %]

tt/lang/ru/user_extra.tti
@@ -7,5 +7,7 @@
     email => 'E-Mail',
     icq_number => 'ICQ',
     home_page => 'Адрес домашней страницы',
+    git_author_name => 'Имя git автора',
+    git_author_email => 'E-mail git автора'
   })
 %]

commit c90b430147be5dba82919108395f940e38a88c9f

User: Fix account creation
Broken by 56a2fdd648b305d61c1e864961697f0b2c1ab669

cgi-bin/CATS/User.pm
@@ -179,7 +179,7 @@ sub insert
     $dbh->do(qq~
         INSERT INTO accounts (
             id, srole, passwd, settings, ~ . join (', ', param_names()) . qq~
-        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)~, {},
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)~, {},
         $aid, $cats::srole_user, $self->{password1}, $new_settings, $self->values
     );
     add_to_contest(contest_id => $_->{id}, account_id => $aid, is_ooc => 1)