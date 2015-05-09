klenin/cats-main

commit 5bd9b8c987b681708acb694f07ae3b2296486092

Add link to the problem's remote url

cgi-bin/CATS/UI/Problems.pm
@@ -117,8 +117,8 @@ sub problems_replace
     my $file = param('zip') || '';
     $file =~ /\.(zip|ZIP)$/
         or return msg(53);
-    my ($contest_id, $old_title) = $dbh->selectrow_array(qq~
-        SELECT contest_id, title FROM problems WHERE id=?~, {}, $pid);
+    my ($contest_id, $old_title, $repo) = $dbh->selectrow_array(qq~
+        SELECT contest_id, title, repo FROM problems WHERE id=?~, {}, $pid);
     # Forbid replacing linked problems. Firstly for robustness,
     # secondly for security -- to avoid checking is_jury($contest_id).
     $contest_id == $cid
@@ -127,7 +127,7 @@ sub problems_replace
 
     my CATS::Problem $p = CATS::Problem->new;
     $p->{old_title} = $old_title unless param('allow_rename');
-    my $error = $p->load(CATS::Problem::Source::Zip->new($fname, $p), $cid, $pid, 1, param('message'), param('is_amend'));
+    my $error = $p->load(CATS::Problem::Source::Zip->new($fname, $p), $cid, $pid, 1, $repo, param('message'), param('is_amend'));
     $t->param(problem_import_log => $p->encoded_import_log());
     #unlink $fname;
     if (!$error) {
@@ -145,7 +145,7 @@ sub problems_replace
 
 sub problems_add
 {
-    my ($source, $source_name) = @_;
+    my ($source_name, $is_remote) = @_;
     my $problem_code;
     if (!$contest->is_practice) {
         ($problem_code) = $contest->unused_problem_codes
@@ -153,7 +153,9 @@ sub problems_add
     }
 
     my CATS::Problem $p = CATS::Problem->new;
-    my $error = $p->load($source->new($source_name, $p), $cid, new_id, 0, 0);
+    my $error = $is_remote
+              ? $p->load(CATS::Problem::Source::Git->new($source_name, $p), $cid, new_id, 0, $source_name)
+              : $p->load(CATS::Problem::Source::Zip->new($source_name, $p), $cid, new_id, 0, undef);
     $t->param(problem_import_log => $p->encoded_import_log());
     $error ||= !add_problem_to_contest($p->{id}, $problem_code);
 
@@ -172,7 +174,7 @@ sub problems_add_new
     $file =~ /\.(zip|ZIP)$/
         or return msg(53);
     my $fname = save_uploaded_file('zip');
-    problems_add('CATS::Problem::Source::Zip', $fname);
+    problems_add($fname, 0);
     unlink $fname;
 }
 
@@ -180,7 +182,7 @@ sub problems_add_new_remote
 {
     my $url = param('remote_url') || '';
     $url or return msg(1091);
-    problems_add('CATS::Problem::Source::Git', $url);
+    problems_add($url, 1);
 }
 
 sub download_problem
@@ -723,7 +725,7 @@ sub problems_frame
     my $test_count_sql = $is_jury ? '(SELECT COUNT(*) FROM tests T WHERE T.problem_id = P.id) AS test_count,' : '';
     my $sth = $dbh->prepare(qq~
         SELECT
-            CP.id AS cpid, P.id AS pid,
+            CP.id AS cpid, P.id AS pid, P.repo,
             ${select_code} AS code, P.title AS problem_name, OC.title AS contest_name,
             ($reqs_count_sql $cats::st_accepted$account_condition) AS accepted_count,
             ($reqs_count_sql $cats::st_wrong_answer$account_condition) AS wrong_answer_count,
@@ -769,6 +771,7 @@ sub problems_frame
     {
         my $c = $_[0]->fetchrow_hashref or return ();
         $c->{status} ||= 0;
+        my $remote_url = defined $c->{repo} && $c->{repo} !~ '\d+' ? $c->{repo} : undef;
         return (
             href_delete => url_f('problems', 'delete' => $c->{cpid}),
             href_change_status => url_f('problems', 'change_status' => $c->{cpid}),
@@ -793,6 +796,7 @@ sub problems_frame
             code => $c->{code},
             problem_name => $c->{problem_name},
             is_linked => $c->{is_linked},
+            remote_url => $remote_url,
             usage_count => $c->{usage_count},
             contest_name => $c->{contest_name},
             accept_count => $c->{accepted_count},

cgi-bin/CATS/Problem.pm
@@ -21,7 +21,7 @@ use fields qw(
     tests testsets samples objects keywords
     imports solutions generators modules pictures attachments
     test_defaults current_tests current_sample gen_groups
-    encoding stml zip zip_archive old_title replace tag_stack has_checker de_list run_method
+    encoding stml zip zip_archive old_title replace repo tag_stack has_checker de_list run_method
 );
 
 use CATS::Problem::Tests;
@@ -61,52 +61,59 @@ sub encoded_import_log
 }
 
 
-sub get_repo
+sub get_repo_id
 {
     my ($id, $sha) = @_;
     my ($db_id, $db_sha) = $dbh->selectrow_array(qq~
        SELECT repo, commit_sha FROM problems WHERE id = ?~, undef, $id);
     my $p = cats_dir() . $cats::repos_dir;
     die 'Repository not found' unless (($db_id ne '' && -d "$p$db_id/") || -d "$p$id/");
-    return $db_id ne '' ? ($db_id, $db_sha) : ($id, $sha // '');
+    return $db_id =~ '/\d+/' ? ($db_id, $db_sha) : ($id, $sha // '');
 }
 
+
+sub get_repo
+{
+    my ($pid, $sha, $need_find, %opts) = @_;
+    ($pid, $sha) = get_repo_id($pid, $sha) if $need_find;
+    $opts{dir} = cats_dir . "$cats::repos_dir$pid/";
+    return CATS::Problem::Repository->new(%opts);
+}
 
 sub get_repo_archive
 {
     my ($pid, $sha) = @_;
-    ($pid) = get_repo($pid);
+    ($pid) = get_repo_id($pid);
     return CATS::Problem::Repository->new(dir => cats_dir . "$cats::repos_dir$pid/")->archive($sha);
 }
 
 
 sub show_commit
 {
-    my ($pid) = get_repo(@_);
-    return CATS::Problem::Repository->new(dir => cats_dir() . "$cats::repos_dir$pid/")->commit_info($_[1], $_[2]);
+    my ($pid, $sha, $enc) = @_;
+    return get_repo($pid, $sha, 1)->commit_info($sha, $enc);
 }
 
 
 sub get_log
 {
     my ($pid, $sha, $max_count) = @_;
-    ($pid, $sha) = get_repo($pid, $sha);
-    return CATS::Problem::Repository->new(
-        dir => cats_dir() . "$cats::repos_dir$pid/")->log(sha => $sha, max_count => $max_count);
+    ($pid, $sha) = get_repo_id(@_);
+    return get_repo($pid, $sha, 0)->log(sha => $sha, max_count => $max_count);
 }
 
 
 sub add_history
 {
     my ($self, $message, $is_amend) = @_;
-    $self->{source}->finalize($dbh, $self, $message, $is_amend, get_repo($self->{id}));
+    $self->{source}->finalize($dbh, $self, $message, $is_amend, get_repo_id($self->{id}));
 }
 
 
 sub load
 {
     my CATS::Problem $self = shift;
-    ($self->{source}, $self->{contest_id}, $self->{id}, $self->{replace}, my $message, my $is_amend) = @_;
+    ($self->{source}, $self->{contest_id}, $self->{id}, $self->{replace}, $self->{repo}, my $message, my $is_amend) = @_;
 
     eval {
         $self->{source}->init;
@@ -171,7 +178,7 @@ sub delete
     else {
         # Cascade into contest_problems and reqs.
         $dbh->do(q~DELETE FROM problems WHERE id = ?~, undef, $pid);
-        CATS::Problem::Repository->new(dir => cats_dir() . "$cats::repos_dir$pid/")->delete;
+        get_repo($pid, undef, 0)->delete;
     }
 
     CATS::StaticPages::invalidate_problem_text(cid => $old_contest, cpid => $cpid);
@@ -750,7 +757,7 @@ sub end_tag_Problem
             title=?, lang=?, time_limit=?, memory_limit=?, difficulty=?, author=?, input_file=?, output_file=?,
             statement=?, pconstraints=?, input_format=?, output_format=?, formal_input=?, json_data=?, explanation=?, zip_archive=?,
             upload_date=CURRENT_TIMESTAMP, std_checker=?, last_modified_by=?,
-            max_points=?, run_method=?, hash=NULL
+            max_points=?, run_method=?, repo=?, hash=NULL
         WHERE id = ?~
     : q~
         INSERT INTO problems (
@@ -758,9 +765,9 @@ sub end_tag_Problem
             title, lang, time_limit, memory_limit, difficulty, author, input_file, output_file,
             statement, pconstraints, input_format, output_format, formal_input, json_data, explanation, zip_archive,
             upload_date, std_checker, last_modified_by,
-            max_points, run_method, id
+            max_points, run_method, repo, id
         ) VALUES (
-            ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP,?,?,?,?,?
+            ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP,?,?,?,?,?,?
         )~;
 
     my $c = $dbh->prepare($sql);
@@ -774,6 +781,7 @@ sub end_tag_Problem
     $c->bind_param($i++, $CATS::Misc::uid);
     $c->bind_param($i++, $self->{problem}->{max_points});
     $c->bind_param($i++, $self->{run_method});
+    $c->bind_param($i++, $self->{repo});
     $c->bind_param($i++, $self->{id});
     $c->execute;
 

cgi-bin/CATS/Problem/Repository.pm
@@ -755,6 +755,14 @@ sub move_history
     return $self;
 }
 
+sub get_remote_url
+{
+    my $self = shift;
+    my ($remote_url) = $self->git('config remote.origin.url');
+    chomp $remote_url;
+    return $remote_url;
+}
+
 sub commit
 {
     my ($self, $problem_author, $message, $is_amend) = @_;

tt/problems.html.tt
@@ -101,6 +101,7 @@ function chg_code(st_sel, pid)
     </select>
     [%- IF p.is_linked %] [<a href="[% p.href_original_contest %]" title="[% p.contest_name %]">L</a>][% END %]
     [%- IF p.usage_count %] [<a href="[% p.href_usage %]" title="[% capt.used_in %]">[% p.usage_count %]</a>][% END %]
+    [%- IF p.remote_url %] [<a href="[% p.remote_url %]" title="[% p.remote_url %]">R</a>][% END %]
 </td>
 <td><a href="[% p.href_select_testsets %]">[% p.testsets %]</a> ([% p.test_count %])
   [%- IF p.points_testsets %] / <a href="[% p.href_select_testsets %]">[% p.points_testsets %]</a>[% END -%]

commit 82ee4889b20c3ab62578402856feb00bbd5b0266

UI::Problems::Fix sorting
Broken by 5bd9b8c987b681708acb694f07ae3b2296486092

cgi-bin/CATS/UI/Problems.pm
@@ -728,7 +728,7 @@ sub problems_frame
     my $test_count_sql = $is_jury ? '(SELECT COUNT(*) FROM tests T WHERE T.problem_id = P.id) AS test_count,' : '';
     my $sth = $dbh->prepare(qq~
         SELECT
-            CP.id AS cpid, P.id AS pid, P.repo,
+            CP.id AS cpid, P.id AS pid,
             ${select_code} AS code, P.title AS problem_name, OC.title AS contest_name,
             ($reqs_count_sql $cats::st_accepted$account_condition) AS accepted_count,
             ($reqs_count_sql $cats::st_wrong_answer$account_condition) AS wrong_answer_count,
@@ -740,7 +740,7 @@ sub problems_frame
             P.upload_date,
             (SELECT A.login FROM accounts A WHERE A.id = P.last_modified_by) AS last_modified_by,
             SUBSTRING(P.explanation FROM 1 FOR 1) AS has_explanation,
-            $test_count_sql CP.testsets, CP.points_testsets, P.lang, P.memory_limit, P.time_limit, CP.max_points
+            $test_count_sql CP.testsets, CP.points_testsets, P.lang, P.memory_limit, P.time_limit, CP.max_points, P.repo
         FROM problems P, contest_problems CP, contests OC
         WHERE CP.problem_id = P.id AND OC.id = P.contest_id AND CP.contest_id = ?$hidden_problems
         ~ . order_by