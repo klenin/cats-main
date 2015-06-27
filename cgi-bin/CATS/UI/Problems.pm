fa3f205a6242d90459f76b27042105a8c368818e
Alexander Klenin <klenin@gmail.com>
2015-06-27 13:32:21 +1000

    history: Move column captions to a proper location

--- cgi-bin/CATS/UI/Problems.pm
+++ cgi-bin/CATS/UI/Problems.pm
@@ -1042,11 +1042,11 @@ sub problem_history_frame
     );
 
     my @cols = (
-        { caption => res_str(1400), width => '25%', order_by => 'author' },
+        { caption => res_str(650), width => '25%', order_by => 'author' },
         { caption => res_str(634),  width => '10%', order_by => 'author_date' },
-        { caption => res_str(1401), width => '10%', order_by => 'committer_date' },
-        { caption => res_str(1402), width => '8%', order_by => 'sha' },
-        { caption => res_str(1403), width => '47%', order_by => 'message' }
+        { caption => res_str(651), width => '10%', order_by => 'committer_date' },
+        { caption => res_str(652), width => '8%', order_by => 'sha' },
+        { caption => res_str(653), width => '47%', order_by => 'message' }
     );
     define_columns(url_f('problem_history', pid => $pid), 1, 0, \@cols);
     my $fetch_record = sub {
