6a23a65b1bf6e7aef40a6176c037879528848d14
TheLocky <xtpvxt@gmail.com>
2016-12-20 15:11:54 +1000

    ApiJudge: Add reading functions
    get_DEs, get_problem, get_problem_sources, get_problem_tests

--- cgi-bin/CATS/ApiJudge.pm
+++ cgi-bin/CATS/ApiJudge.pm
@@ -39,4 +39,61 @@ sub update_state {
     print_json({ lock_counter => $lock_counter, is_alive => $is_alive });
 }
 
+sub get_DEs {
+    $sid or print_json({ error => "bad sid"});
+
+    my $db_de = $dbh->selectall_arrayref(q~
+        SELECT id, code, description, memory_handicap FROM default_de~, { Slice => {} });
+
+    print_json({ db_de => $db_de });
+}
+
+sub get_problem {
+    $sid or print_json({ error => "bad sid"});
+    my ($p) = @_;
+
+    my $problem = $dbh->selectrow_hashref(q~
+        SELECT
+            id, title, upload_date, time_limit, memory_limit,
+            input_file, output_file, std_checker, contest_id, formal_input,
+            run_method
+        FROM problems WHERE id = ?~, { Slice => {} }, $p->{pid});
+    $problem->{run_method} //= $cats::rm_default;
+
+    print_json({ problem => $problem });
+}
+
+sub get_problem_sources {
+    $sid or print_json({ error => "bad sid"});
+    my ($p) = @_;
+
+    my $problem_sources = $dbh->selectall_arrayref(q~
+        SELECT ps.*, dd.code FROM problem_sources ps
+            INNER JOIN default_de dd ON dd.id = ps.de_id
+        WHERE ps.problem_id = ? ORDER BY ps.id~, { Slice => {} },
+        $p->{pid});
+
+    my $imported = $dbh->selectall_arrayref(q~
+        SELECT ps.*, dd.code FROM problem_sources ps
+            INNER JOIN default_de dd ON dd.id = ps.de_id
+            INNER JOIN problem_sources_import psi ON ps.guid = psi.guid
+        WHERE psi.problem_id = ? ORDER BY ps.id~, { Slice => {} },
+        $p->{pid});
+    $_->{is_imported} = 1 for @$imported;
+
+    print_json({ sources => [ @$problem_sources, @$imported ] });
+}
+
+sub get_problem_tests {
+    $sid or print_json({ error => "bad sid"});
+    my ($p) = @_;
+
+    my $tests = $dbh->selectall_arrayref(q~
+        SELECT generator_id, input_validator_id, rank, param, std_solution_id, in_file, out_file, gen_group
+        FROM tests WHERE problem_id = ? ORDER BY rank~, { Slice => {} },
+        $p->{pid});
+
+    print_json({ tests => $tests });
+}
+
 1;
