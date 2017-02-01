570564b27bd90a20057e2ce7980d4750b9be9b3e
Alexander Klenin <klenin@gmail.com>
2017-02-02 02:08:32 +1000

    sources_info: Add menu

--- cgi-bin/CATS/Misc.pm
+++ cgi-bin/CATS/Misc.pm
@@ -228,7 +228,6 @@ sub generate_output
         dbi_profile => $dbh->{Profile}->{Data}->[0],
         #dbi_profile => Data::Dumper::Dumper($dbh->{Profile}->{Data}),
         langs => [ map { href => url_f('contests', lang => $_), name => $_ }, @cats::langs ],
-        lang => lang(),
     );
 
     my $elapsed_minutes = int(($contest->{time_since_start} - $virtual_diff_time) * 1440);
