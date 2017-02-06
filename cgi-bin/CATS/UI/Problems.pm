46710d6a6502961e63ab071753e3bb2951e202c5
Alexander Klenin <klenin@gmail.com>
2017-02-06 19:19:59 +1000

    problems: Display 'log in to participate' message

--- cgi-bin/CATS/UI/Problems.pm
+++ cgi-bin/CATS/UI/Problems.pm
@@ -686,6 +686,7 @@ sub problems_frame {
         { href => url_f('contests', params => $cid), item => res_str(546) };
     $t->param(
         href_action => url_f('problems'),
+        href_login => url_f('login'),
         contest_descr => $contest->{short_descr},
         submenu => \@submenu, title_suffix => res_str(525),
         is_team => $my_is_team, is_practice => $contest->is_practice,
