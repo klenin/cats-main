9676d57f34bc93ae4b3f65c08d50f7210035cca3
JIukaviy <jiukaviy@gmail.com>
2017-02-17 19:33:20 +1000

    Problems: Add clone_req function, and export it

--- cgi-bin/CATS/UI/Problems.pm
+++ cgi-bin/CATS/UI/Problems.pm
@@ -23,6 +23,12 @@ use CATS::StaticPages;
 use CATS::Utils qw(url_function file_type date_to_iso);
 use CATS::Web qw(param url_param redirect upload_source);
 
+use Exporter qw(import);
+
+our @EXPORT = qw(
+    clone_req
+);
+
 sub problems_change_status
 {
     my $cpid = param('change_status')
@@ -95,6 +101,34 @@ sub insert_req {
         undef,
         $rid, 1, $submit_uid, CATS::IP::get_ip);
     $rid;
+}
+
+sub clone_req {
+    my ($element_req_id, $submit_uid) = @_;
+
+    $element_req_id = $dbh->selectrow_array(q~
+        SELECT RG.element_id FROM req_groups RG
+        WHERE RG.group_id = ?~, undef,
+        $element_req_id) || $element_req_id;
+
+    my $req = $dbh->selectrow_hashref(q~
+        SELECT R.problem_id, R.contest_id FROM reqs R
+        WHERE R.id = ?~, undef,
+        $element_req_id);
+
+    my $group_req_id = insert_req($req->{problem_id}, $submit_uid, $cats::st_not_processed, $req->{contest_id});
+
+    $dbh->do(q~
+        INSERT INTO req_groups (
+            id, element_id, group_id
+        ) VALUES (
+            ?, ?, ?
+        )~, {},
+        new_id, $element_req_id, $group_req_id);
+
+    $dbh->commit;
+
+    $group_req_id;
 }
 
 sub determine_state {
