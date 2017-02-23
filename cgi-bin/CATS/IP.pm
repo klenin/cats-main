2b98f5ab35e59bb2356bc3e92ddd9c3cee037c53
Alexander Klenin <klenin@gmail.com>
2017-02-23 23:32:17 +1000

    IP: Use Config::ip_aliases instead of hard-coded list

--- cgi-bin/CATS/IP.pm
+++ cgi-bin/CATS/IP.pm
@@ -7,36 +7,23 @@ use List::Util qw(first);
 
 use CATS::Config;
 
-sub filter_ip
-{
+sub filter_ip {
     my ($ip) = @_;
     defined $ip or return undef;
-    for ($ip)
-    {
-        #s/195.209.254.222/pin/;
-        #s/195.209.254.4/www/;
-        #s/195.209.254.3/proxy/;
-        #s/77.234.208.5/dvgu/;
-        #s/77.234.208.4/proxy/;
-        #s/192.168.9.5/dvgu/;
-        #s/10.0.0.5/nato/;
-        s/10.101.1.135/www/;
+    for (keys %CATS::Config::ip_aliases) {
+        return $_ if $ip =~ $CATS::Config::ip_aliases{$_};
     }
     $ip;
 }
 
-
-sub get_ip
-{
+sub get_ip {
    join ', ', grep $_ ne '',
        ($ENV{HTTP_X_FORWARDED_FOR} || ''), ($ENV{REMOTE_ADDR} || '');
 }
 
-
 sub local_ip { $_[0] =~ /(\d+)\.(\d+)\.(\d+)\.(\d+)/ ? $1 == 10 || $1 == 192 && $2 == 168 : 1; }
 
-sub linkify_ip
-{
+sub linkify_ip {
     my ($ip) = $_[0] || '';
     my (@ips) = split /[,\s]+/, $ip;
     my $short = (first { !local_ip($_) } @ips) || $ips[0] || '';
