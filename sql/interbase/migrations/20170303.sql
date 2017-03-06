257a5a14ea9da256e6be88973903b4ae5c2e965c
Alexander Klenin <klenin@gmail.com>
2017-03-06 13:53:04 +1000

    migrations: Fix lock_counter transition

--- sql/interbase/migrations/20170303.sql
+++ sql/interbase/migrations/20170303.sql
@@ -4,6 +4,6 @@ ALTER TABLE judges ADD CONSTRAINT chk_judge_pin_mode
   CHECK (pin_mode IN (0, 1, 2, 3));
 ALTER TABLE judges ALTER pin_mode POSITION 4;
 COMMIT;
-UPDATE judges SET pin_mode = (1 - lock_counter) * 3;
+UPDATE judges SET pin_mode = (1 + lock_counter) * 3;
 COMMIT;
 ALTER TABLE judges DROP lock_counter;
