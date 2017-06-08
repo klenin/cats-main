e58c566ca3bb21cc8b4d50623e41aadde7f872db
Alexander Klenin <klenin@gmail.com>
2017-06-09 00:19:32 +1000

    Judges: Display submissions count

--- cgi-bin/CATS/UI/Judges.pm
+++ cgi-bin/CATS/UI/Judges.pm
@@ -88,23 +88,30 @@ sub judges_frame {
     }
 
     $lv->define_columns(url_f('judges'), 0, 0, [
-        { caption => res_str(625), order_by => '2', width => '30%' },
-        ($is_root ? ({ caption => res_str(616), order_by => '3', width => '30%' }) : ()),
+        { caption => res_str(625), order_by => '2', width => '25%' },
+        ($is_root ? ({ caption => res_str(616), order_by => '3', width => '25%' }) : ()),
+        ($is_root ? ({ caption => res_str(649), order_by => '10', width => '10%' }) : ()),
         { caption => res_str(626), order_by => '4', width => '10%' },
         { caption => res_str(633), order_by => '5', width => '15%' },
         { caption => res_str(622), order_by => '6', width => '10%' },
     ]);
     $lv->define_db_searches([ qw(J.id nick login is_alive alive_date pin_mode account_id last_ip) ]);
 
-    my $c = $dbh->prepare(q~
-        SELECT J.id, J.nick, A.login, J.is_alive, J.alive_date, J.pin_mode, A.id, A.last_ip
-            FROM judges J LEFT JOIN accounts A ON A.id = J.account_id WHERE 1 = 1 ~ .
-            $lv->maybe_where_cond . $lv->order_by);
+    my $req_counts = $is_root ? qq~,
+        (SELECT COUNT(*) FROM reqs R WHERE R.judge_id = J.id AND R.state = $cats::st_testing),
+        (SELECT COUNT(*) FROM reqs R WHERE R.judge_id = J.id)~ : '';
+
+    my $c = $dbh->prepare(qq~
+        SELECT
+            J.id, J.nick, A.login, J.is_alive, J.alive_date, J.pin_mode, A.id, A.last_ip$req_counts
+        FROM judges J LEFT JOIN accounts A ON A.id = J.account_id WHERE 1 = 1 ~ .
+        $lv->maybe_where_cond . $lv->order_by);
     $c->execute($lv->where_params);
 
     my $fetch_record = sub {
         my (
-            $jid, $judge_name, $account_name, $is_alive, $alive_date, $pin_mode, $account_id, $last_ip
+            $jid, $judge_name, $account_name, $is_alive, $alive_date, $pin_mode, $account_id, $last_ip,
+            $processing_count, $processed_count
         ) = $_[0]->fetchrow_array or return ();
         return (
             jid => $jid,
@@ -114,10 +121,14 @@ sub judges_frame {
             pin_mode => $pin_mode,
             is_alive => $is_alive,
             alive_date => $alive_date,
+            processing_count => $processing_count,
+            processed_count => $processed_count,
+
             href_ping => url_f('judges', ping => $jid),
             href_edit => url_f('judges', edit => $jid),
             href_delete => url_f('judges', 'delete' => $jid),
             href_account => url_f('users', edit => $account_id),
+            href_console => url_f('console', search => "judge_id=$jid,state=P", se => 'judge', i_value => -1, show_results => 1),
         );
     };
 
@@ -126,7 +137,7 @@ sub judges_frame {
     $t->param(submenu => [ references_menu('judges') ], editable => $is_root);
 
     my ($not_processed) = $dbh->selectrow_array(q~
-        SELECT COUNT(*) FROM reqs WHERE state = ?~, undef,
+        SELECT COUNT(*) FROM reqs WHERE state = ? AND judge_id IS NULL~, undef,
         $cats::st_not_processed);
     $t->param(not_processed => $not_processed);
 }
