d9708f1c90da5cf9c9d28488a9bb48d986dc20f6
Alexander Klenin <klenin@gmail.com>
2017-06-16 22:17:43 +1000

    Judge: Do not include request-pinned judges in total count

--- cgi-bin/CATS/Judge.pm
+++ cgi-bin/CATS/Judge.pm
@@ -18,8 +18,8 @@ sub ping {
 sub get_active_count {
     $dbh->selectrow_array(qq~
         SELECT SUM(CASE WHEN CURRENT_TIMESTAMP - J.alive_date < ? THEN 1 ELSE 0 END), COUNT(*)
-            FROM judges J WHERE J.pin_mode <> ?~, undef,
-        3 * $CATS::Config::judge_alive_interval / 24, $cats::judge_pin_locked);
+            FROM judges J WHERE J.pin_mode > ?~, undef,
+        3 * $CATS::Config::judge_alive_interval / 24, $cats::judge_pin_req);
 }
 
 1;
