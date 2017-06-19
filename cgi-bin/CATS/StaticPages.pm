4616f6eadbbe8da9ff43d2ae5a6d908723abebe5
Alexander Klenin <klenin@gmail.com>
2017-06-19 21:20:27 +1000

    StaticPages: Minor style fixes

--- cgi-bin/CATS/StaticPages.pm
+++ cgi-bin/CATS/StaticPages.pm
@@ -8,8 +8,7 @@ use CATS::DB;
 use CATS::Misc qw($sid);
 use CATS::Web qw(log_info url_param restore_parameters);
 
-sub allowed_pages
-{{
+sub allowed_pages {{
     problem_text => { cid => 1, cpid => 1, pid => 1 },
     rank_table_content => { cid => 1, hide_ooc => 1, printable => 1 },
 }}
@@ -18,8 +17,7 @@ our $is_static_page;
 
 sub is_static_page { $is_static_page = (url_param('f') || '') eq 'static' }
 
-sub process_static
-{
+sub process_static {
     my $url = $ENV{REDIRECT_URL};
     my ($f, $p) = $url =~ /^\/\w+\/static\/([a-z_]+)-([a-z_\-0-9]+)\.html/;
     $f && $p or die $url;
@@ -34,21 +32,16 @@ sub process_static
     return $output_file;
 }
 
-
-sub name
-{
+sub name {
     my ($f, %p) = @_;
     "$f-" . join('-', map "$_-$p{$_}", sort keys %p);
 }
-
 
 sub url_static { './static/' . name(@_) . '.html' . ($sid ? "?sid=$sid" : ''); }
 sub path { cats_dir() . '../static/' }
 sub full_name { path() . name(@_) . '.html' }
 
-
-sub invalidate_problem_text
-{
+sub invalidate_problem_text {
     my (%p) = @_;
     my @contest_ids = $p{cid} ? ($p{cid}) : ();
     my @cpids = $p{cpid} ? ($p{cpid}) : ();
@@ -71,6 +64,5 @@ sub invalidate_problem_text
     unlink full_name('problem_text', cid => $_) for @contest_ids;
     unlink full_name('problem_text', cpid => $_) for @cpids;
 }
-
 
 1;
