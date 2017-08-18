d85cd4786ca64ee6d22f21755eb924f4f000f425
Alexander Klenin <klenin@gmail.com>
2017-08-18 18:13:49 +1000

    Remove obsolete 'archive' script

--- archive.sh
+++
@@ -1,5 +0,0 @@
-tar -c -z -f `date "+cats_%Y_%m_%d.tar.gz"` \
---exclude 'backup/*' --exclude 'static/*' --exclude '*.gz' --exclude '*.gdb' --exclude '*.fdb' --exclude 'judge/solutions/*' \
---exclude '*.exe' --exclude 'cgi-bin/download/*' --exclude '*.pch' --exclude '*.pdb' \
---exclude '*.ilk' --exclude '*.obj' --exclude '*.log' --exclude '*.idb' \
---exclude '*.zip' --exclude '*.ncb' --exclude 'rank_cache/*' *
