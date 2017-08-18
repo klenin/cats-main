d3e6355ecb5b26241c06581dc1589cffeb2cc611
Alexander Klenin <klenin@gmail.com>
2017-08-19 00:06:30 +1000

    sql: Add 'judge' role

---
+++ sql/interbase/init_roles.sql
@@ -0,0 +1,56 @@
+CREATE ROLE judge;
+
+GRANT SELECT ON TABLE default_de TO judge;
+GRANT SELECT ON TABLE accounts TO judge;
+GRANT SELECT ON TABLE judges TO judge;
+GRANT SELECT ON TABLE judge_de_bitmap_cache TO judge;
+GRANT SELECT ON TABLE contests TO judge;
+GRANT SELECT ON TABLE contest_accounts TO judge;
+GRANT SELECT ON TABLE limits TO judge;
+GRANT SELECT ON TABLE problems TO judge;
+GRANT SELECT ON TABLE problem_de_bitmap_cache TO judge;
+GRANT SELECT ON TABLE contest_problems TO judge;
+GRANT SELECT ON TABLE problem_sources TO judge;
+GRANT SELECT ON TABLE problem_sources_import TO judge;
+GRANT SELECT ON TABLE tests TO judge;
+GRANT SELECT ON TABLE testsets TO judge;
+GRANT SELECT ON TABLE reqs TO judge;
+GRANT SELECT ON TABLE req_de_bitmap_cache TO judge;
+GRANT SELECT ON TABLE req_groups TO judge;
+GRANT SELECT ON TABLE req_details TO judge;
+GRANT SELECT ON TABLE solution_output TO judge;
+GRANT SELECT ON TABLE log_dumps TO judge;
+GRANT SELECT ON TABLE sources TO judge;
+
+GRANT UPDATE(sid, last_login, last_ip)
+    ON TABLE accounts TO judge;
+
+GRANT UPDATE(is_alive, alive_date)
+    ON TABLE judges TO judge;
+
+GRANT INSERT, UPDATE
+    ON TABLE judge_de_bitmap_cache TO judge;
+
+GRANT DELETE, INSERT, UPDATE
+    ON TABLE problem_de_bitmap_cache TO judge;
+
+GRANT UPDATE(status)
+    ON TABLE contest_problems TO judge;
+
+GRANT UPDATE(in_file, in_file_size, out_file, out_file_size)
+    ON TABLE tests TO judge;
+
+GRANT UPDATE(state, failed_test, result_time, judge_id)
+    ON TABLE reqs TO judge;
+
+GRANT DELETE, INSERT
+    ON TABLE req_de_bitmap_cache TO judge;
+
+GRANT DELETE, INSERT
+    ON TABLE req_details TO judge;
+
+GRANT INSERT
+    ON TABLE solution_output TO judge;
+
+GRANT INSERT, UPDATE
+    ON TABLE log_dumps TO judge;
