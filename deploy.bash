3fd97843efdd726dd8f5d66778fce6f1fa49e476
KeltarHelviett <keltarhelviett@gmail.com>
2018-03-31 06:46:00 +1000

    Improved installation via deploy.sh

--- deploy.bash
+++ deploy.bash
@@ -41,12 +41,43 @@ CATS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # assume, that scr
 # Group, apache running under
 http_group=www-data
 
+
+
+FB_DEV_VERSION=`sudo apt-cache pkgnames | grep firebird-dev`
+FB_DEF_OP_MODE='superclassic'
+packages=(firebird-dev)
+
+# The firebird-dev package may contain different verisons of Firebird.
+# It depends on platform you're using.
+# Packages firebird2.*-(classic|super|superclassic) are mutually exclusive.
+# Firebird 3.0 and higher allows to switch Operation Mode in the
+# /etc/firebird/3.*/firebird.conf
+
 echo "1. Install apt packages... "
 if [[ $step =~ (^|,)1(,|$)  || $step == "*" ]]; then
-	packages=(git firebird2.5-dev firebird2.5-classic build-essential libaspell-dev
+	
+	if [[ $FB_DEV_VERSION ]]; then
+		FB_DEV_VERSION=`sudo apt-cache show firebird-dev | grep Version`
+
+		if [[ $FB_DEV_VERSION =~ ([0-9]+\.[0-9]+) ]]
+		then
+			FB_DEV_VERSION=${BASH_REMATCH[1]}
+			if [[ `echo "$FB_DEV_VERSION < 3.0" | bc` -eq 1 ]]; then
+				read -e -p "Firebird Operation Mode (classic, super, superclassic): " -i $FB_DEF_OP_MODE $FB_DEF_OP_MODE
+				FB_PACKAGE=firebird${FB_DEV_VERSION}-${FB_DEF_OP_MODE}
+			else
+				FB_PACKAGE=firebird${FB_DEV_VERSION}-server
+			fi
+			packages+=($FB_PACKAGE)
+		else
+			echo "Can't find a proper firebird-dev package"
+		fi
+	fi 
+	packages+=(git build-essential libaspell-dev
 		aspell-en aspell-ru apache2 libapache2-mod-perl2 libapreq2-3 libapreq2-dev
 		libapache2-mod-perl2-dev libexpat1 libexpat1-dev libapache2-request-perl cpanminus)
 	sudo apt-get -y install ${packages[@]}
+	sudo dpkg-reconfigure $FB_PACKAGE # In some cases default dialog just doesn't configure SYSDBA user
 	echo "ok"
 else
 	echo "skip"
@@ -176,18 +207,19 @@ EOF
 	chmod -R g+r cgi-bin
 	chmod g+rw static tt download/{,att,img,pr,vis} cgi-bin/rank_cache{,/r}
 	sudo service apache2 reload
+	sudo service apache2 restart
 	echo "ok"
 else
 	echo "skip"
 fi
 
 echo "7. Configure and init cats database... "
-if [[ $step =~ (^|,)7(,|$) || $step == "*" ]]; then
+if [[ ($step =~ (^|,)7(,|$) || $step == "*") && $FB_DEV_VERSION ]]; then
 	CONFIG_NAME="Config.pm"
 	CONFIG_ROOT="${CATS_ROOT}/cgi-bin/cats-problem/CATS"
 	CREATE_DB_NAME="create_db.sql"
 	CREATE_DB_ROOT="${CATS_ROOT}/sql/interbase"
-	FB_ALIASES="/etc/firebird/2.5/aliases.conf"
+	FB_ALIASES="/etc/firebird/${FB_DEV_VERSION}/aliases.conf"
 
 	CONFIG="$CONFIG_ROOT/$CONFIG_NAME"
 	cp "$CONFIG_ROOT/${CONFIG_NAME}.template" $CONFIG
