757014c7f8b7d596cf361f76de6da89e118ecf7e
Alexander Klenin <klenin@gmail.com>
2018-06-18 04:46:45 +1000

    jobs: Display all time fields

--- cgi-bin/CATS/UI/Jobs.pm
+++ cgi-bin/CATS/UI/Jobs.pm
@@ -12,6 +12,7 @@ use CATS::Output qw(init_template url_f);
 use CATS::Utils qw(url_function);
 use CATS::Request;
 use CATS::ReqDetails;
+use CATS::Time;
 
 sub job_details_frame {
     my ($p) = @_;
@@ -37,14 +38,17 @@ sub jobs_frame {
         { caption => res_str(642), order_by => 'type', width => '5%' },
         { caption => res_str(619), order_by => 'id', width => '5%' },
         { caption => res_str(622), order_by => 'state', width => '5%' },
-        { caption => res_str(632), order_by => 'start_time', width => '10%' },
+        { caption => 'create_time', order_by => 'create_time', width => '5%', col => 'Tc' },
+        { caption => 'start_time' , order_by => 'start_time' , width => '5%', col => 'Ts' },
+        { caption => 'finish_time', order_by => 'finish_time', width => '5%', col => 'Tf' },
+        { caption => 'time_len'   , order_by => 'time_len'   , width => '5%', col => 'Tt' },
         { caption => res_str(673), order_by => 'judge_name', width => '10%', col => 'Jn' },
         { caption => res_str(602), order_by => 'problem_title', width => '15%', col => 'Pr' },
         { caption => res_str(603), order_by => 'contest_title', width => '15%', col => 'Ct' },
         { caption => res_str(608), order_by => 'team_name', width => '15%', col => 'Ac' },
     ]);
     $lv->define_db_searches([ qw(
-        id type state start_time judge_id judge_name
+        id type state create_time start_time finish_time time_len judge_id judge_name
         problem_id problem_title J1.contest_id contest_title account_id team_name req_id
         in_queue
     ) ]);
@@ -76,7 +80,8 @@ sub jobs_frame {
         ($lv->visible_cols->{Jn} ? ' LEFT JOIN judges JD ON J.judge_id = JD.id' : '') .
         ($lv->visible_cols->{Pr} || $lv->visible_cols->{Ct} || $lv->visible_cols->{Ac} ?
             ' LEFT JOIN reqs R ON J.req_id = R.id' : ''),
-        [ qw(J.id J.type J.state J.start_time J.req_id J.judge_id),
+        [ qw(J.id J.type J.state J.create_time J.start_time J.finish_time J.req_id J.judge_id),
+          'CAST(J.finish_time - J.start_time AS DOUBLE PRECISION) AS time_len',
           'CASE WHEN JQ.id IS NULL THEN 0 ELSE 1 END AS in_queue',
           ($lv->visible_cols->{Jn} ?  'JD.nick' : 'NULL') . ' AS judge_name',
           $maybe_field->('Pr', 'problem_id'),
@@ -105,6 +110,7 @@ sub jobs_frame {
         my $row = $_[0]->fetchrow_hashref or return ();
         return (
             %$row,
+            time_len_fmt => CATS::Time::format_diff($row->{time_len}, seconds => 1),
             href_details => (
                 (grep $row->{type} == $_, $cats::job_type_submission, $cats::job_type_submission_part) ?
                 url_f('run_log', rid => $row->{req_id}) . "#job$row->{id}" :
