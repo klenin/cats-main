4f7c38655c8b56e2caca25e0fa445bf348040d42
IlyaDonskoy <donskoi.ia@students.dvfu.ru>
2018-06-29 20:50:44 +1000

    view_source.html.tt: use ace.js instead of synhighlight

---
+++ js/editor.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $('textarea[data-editor]').each(function() {
+   
+    if (!ace) return;
+    var textarea = $(this);
+    var mode = textarea.data('editor');
+    var editDiv = $('<div>', {
+      position: 'relative',
+      width: textarea.width(),
+      height: textarea.height(),
+      'class': 'editor',
+    }).insertBefore(textarea);
+
+    textarea.css('display', 'none');
+    var editor = ace.edit(editDiv[0]);
+    editor.renderer.setShowGutter(textarea.data('gutter'));
+    editor.getSession().setValue(textarea.val());
+    editor.getSession().setMode('ace/mode/' + mode);
+    editor.setTheme('ace/theme/chrome');
+
+    editor.setOptions({
+        fontSize: '12pt',
+    });
+
+  });
+});
