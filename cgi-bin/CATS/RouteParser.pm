1caf8b8251f825294e644f6a2342d49a2f4b8724
Alexander Klenin <klenin@gmail.com>
2018-07-05 12:45:21 +1000

    Router: Extract RouteParser

---
+++ cgi-bin/CATS/RouteParser.pm
@@ -0,0 +1,115 @@
+package CATS::RouteParser;
+
+use strict;
+use warnings;
+
+use Exporter qw(import);
+
+use CATS::Utils;
+
+our @EXPORT = qw(
+    array_of
+    bool
+    bool0
+    check_encoding
+    clist_of
+    encoding
+    encoding_default
+    fixed
+    ident
+    integer
+    problem_code
+    required
+    sha
+    str
+    upload
+);
+
+sub bool() { qr/^1$/ }
+sub bool0() { qr/^[01]$/ }
+sub integer() { qr/^[0-9]+$/ } # 'int' is reserved.
+sub fixed() { qr/^[+-]?([0-9]*[.])?[0-9]+$/ }
+sub sha() { qr/^[a-h0-9]+$/ }
+sub str() { qr/./ }
+sub ident() { qr/^[a-zA-Z][a-zA-Z_0-9]*$/ }
+sub problem_code() { qr/^[A-Za-z0-9]{1,3}$/ }
+
+sub check_encoding { $_[0] && CATS::Utils::encodings->{$_[0]} }
+sub encoding() { \&check_encoding }
+sub encoding_default($) {
+    my ($default) = @_;
+    sub { check_encoding($_[0]) ? $_[0] : ($_[0] = $default) };
+}
+
+sub upload() {{ upload => 1 }}
+
+BEGIN {
+    for my $name (qw(array_of clist_of required)) {
+        no strict 'refs';
+        # Prototype to allow acting as a unary operator.
+        *$name = sub($) {
+            ref $_[0] eq 'HASH' or return { $name => 1, type => $_[0] };
+            $_[0]->{$name} = 1;
+            $_[0];
+        };
+    }
+}
+
+sub check_type { !defined $_[1] || $_[0] =~ $_[1] }
+
+sub parse_route {
+    my ($p, $route) = @_;
+    ref $route eq 'ARRAY' or return $route;
+    my $fn = $route->[0];
+
+    for (my $i = 1; $i < @$route; $i += 2) {
+        my $name = $route->[$i];
+        my $type = $route->[$i + 1];
+
+        if (ref $type eq 'CODE') {
+            my $value = $p->web_param($name);
+            $p->{$name} = $value if $type->($value);
+            next;
+        }
+        if (ref $type ne 'HASH') {
+            my $value = $p->web_param($name);
+            $p->{$name} = $value if defined $value && check_type($value, $type);
+            next;
+        }
+
+        if ($type->{upload}) {
+            $p->{$name} = $p->make_upload($name);
+            next;
+        }
+        if ($type->{array_of}) {
+            my @values = grep check_type($_, $type->{type}), $p->web_param($name);
+            return if !@values && $type->{required};
+            $p->{$name} = \@values;
+            next;
+        }
+
+        my $value = $p->web_param($name);
+        if ($type->{clist_of}) {
+            my @values = grep check_type($_, $type->{type}), split ',', $value // '';
+            return if !@values && $type->{required};
+            $p->{$name} = \@values;
+            next;
+        }
+
+        if (!defined $value) {
+            return if $type->{required};
+            next;
+        }
+
+        if (check_type($value, $type->{type})) {
+            $p->{$name} = $value;
+        }
+        elsif ($type->{required}) {
+            return;
+        }
+    }
+
+    $fn;
+}
+
+1;
--- cgi-bin/CATS/Router.pm
+++ cgi-bin/CATS/Router.pm
@@ -8,46 +8,14 @@ use File::Spec;
 
 use CATS::ApiJudge;
 use CATS::Utils;
+use CATS::RouteParser;
 use CATS::Web qw(url_param param);
 
 BEGIN {
     require $_ for glob File::Spec->catfile($ENV{CATS_DIR} || $FindBin::Bin, 'CATS', 'UI', '*.pm');
 }
 
-sub bool() { qr/^1$/ }
-sub bool0() { qr/^[01]$/ }
-sub integer() { qr/^[0-9]+$/ } # 'int' is reserved.
-sub fixed() { qr/^[+-]?([0-9]*[.])?[0-9]+$/ }
-sub sha() { qr/^[a-h0-9]+$/ }
-sub str() { qr/./ }
-sub ident() { qr/^[a-zA-Z][a-zA-Z_0-9]*$/ }
-sub problem_code { qr/^[A-Za-z0-9]{1,3}$/ }
-
-sub check_encoding { $_[0] && CATS::Utils::encodings->{$_[0]} }
-sub encoding() { \&check_encoding }
-sub encoding_default($) {
-    my ($default) = @_;
-    sub { check_encoding($_[0]) ? $_[0] : ($_[0] = $default) };
-}
-
-sub upload() {{ upload => 1 }}
-
 my ($main_routes, $api_judge_routes);
-
-BEGIN {
-    for my $name (qw(array_of clist_of required)) {
-        no strict 'refs';
-        # Prototype to allow acting as a unary operator.
-        *$name = sub($) {
-            ref $_[0] eq 'HASH' or return { $name => 1, type => $_[0] };
-            $_[0]->{$name} = 1;
-            $_[0];
-        };
-    }
-}
-
-# Separate BEGIN block to allow prototypes to have effect.
-BEGIN {
 
 my %console_params = (
     selection => clist_of integer,
@@ -406,11 +374,7 @@ $api_judge_routes = {
     ],
 };
 
-}
-
 sub parse_uri { $_[0]->get_uri =~ m~/cats/(|main.pl)$~ }
-
-sub check_type { !defined $_[1] || $_[0] =~ $_[1] }
 
 sub common_params {
     my ($p) = @_;
@@ -433,57 +397,7 @@ sub route {
         $main_routes->{$p->{f}} ||
         $api_judge_routes->{$p->{f}}
         or return $default_route;
-    ref $route eq 'ARRAY' or return $route;
-    my $fn = $route->[0];
-
-    for (my $i = 1; $i < @$route; $i += 2) {
-        my $name = $route->[$i];
-        my $type = $route->[$i + 1];
-
-        if (ref $type eq 'CODE') {
-            my $value = param($name);
-            $p->{$name} = $value if $type->($value);
-            next;
-        }
-        if (ref $type ne 'HASH') {
-            my $value = param($name);
-            $p->{$name} = $value if defined $value && check_type($value, $type);
-            next;
-        }
-
-        if ($type->{upload}) {
-            $p->{$name} = $p->make_upload($name);
-            next;
-        }
-        if ($type->{array_of}) {
-            my @values = grep check_type($_, $type->{type}), param($name);
-            return $default_route if !@values && $type->{required};
-            $p->{$name} = \@values;
-            next;
-        }
-
-        my $value = param($name);
-        if ($type->{clist_of}) {
-            my @values = grep check_type($_, $type->{type}), split ',', $value // '';
-            return $default_route if !@values && $type->{required};
-            $p->{$name} = \@values;
-            next;
-        }
-
-        if (!defined $value) {
-            return $default_route if $type->{required};
-            next;
-        }
-
-        if (check_type($value, $type->{type})) {
-            $p->{$name} = $value;
-        }
-        elsif ($type->{required}) {
-            return $default_route;
-        }
-    }
-
-    $fn;
+    CATS::RouteParser::parse_route($p, $route) // $default_route;
 }
 
 1;
