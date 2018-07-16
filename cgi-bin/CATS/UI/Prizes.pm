029cb2fea23ff56b637f194741b6ba8a414dda90
Alexander Klenin <klenin@gmail.com>
2018-07-16 21:10:04 +1000

    Prizes: Use Router

--- cgi-bin/CATS/UI/Prizes.pm
+++ cgi-bin/CATS/UI/Prizes.pm
@@ -10,51 +10,58 @@ use CATS::ListView;
 use CATS::Messages qw(msg res_str);
 use CATS::Output qw(init_template url_f);
 use CATS::References;
-use CATS::Web qw(param url_param);
+use CATS::RouteParser;
 
 sub contest_groups_fields () { qw(name clist) }
-
-sub sanitize_clist { sort { $a <=> $b } grep /^\d+$/, @_ }
 
 sub prizes_edit_frame {
     my ($p) = @_;
     init_template($p, 'prizes_edit.html.tt');
 
-    my $cgid = url_param('edit') or return;
     my $cg = $dbh->selectrow_hashref(q~
         SELECT * FROM contest_groups WHERE id = ?~, undef,
-        $cgid);
+        $p->{edit});
     my $prizes = $dbh->selectall_arrayref(q~
         SELECT * FROM prizes WHERE cg_id = ?~, { Slice => {} },
-        $cgid);
+        $p->{edit});
     $t->param(%$cg, prizes => $prizes, href_action => url_f('prizes'));
 }
 
-sub prize_params { map { $_ => param($_ . '_' . $_[0]) } qw(rank name) }
+sub pluck { map $_->{$_[0]}, @{$_[1]} }
+
+sub prize_params {
+    my ($p, $id) = @_;
+    map { $_ => $p->{"$_-$id"} } qw(rank name);
+}
 
 sub prizes_edit_save {
-    my $cgid = param('id') or return;
-    my %cg = map { $_ => (param($_) || '') } contest_groups_fields;
+    my ($p) = @_;
+    my $cgid = $p->{id} or return;
 
-    my @clist = sanitize_clist split ',', $cg{clist};
+    my @clist = sort { $a <=> $b } @{$p->{clist}};
     @clist && @clist < 100 or return;
-    $cg{clist} = join ',', @clist;
-    return msg(1090) if $cgid != (CATS::Contest::Utils::contest_group_by_clist($cg{clist}) // 0);
+    my $clist = join ',', @clist;
 
-    $dbh->do(_u $sql->update('contest_groups', \%cg, { id => $cgid }));
+    return msg(1090) if $cgid != (CATS::Contest::Utils::contest_group_by_clist($clist) // 0);
+
     my $prizes = $dbh->selectall_arrayref(q~
         SELECT * FROM prizes WHERE cg_id = ?~, { Slice => {} },
         $cgid);
-    for my $p (@$prizes) {
-        my %new = prize_params($p->{id});
+    CATS::RouteParser::parse_route($p, [ 1,
+        map { +"rank-$_" => integer, "name-$_" => str } pluck(id => $prizes), 'new' ]) or return;
+
+    $dbh->do(_u $sql->update('contest_groups',
+        { name => $p->{name} // '', clist => $clist }, { id => $cgid }));
+    for my $pr (@$prizes) {
+        my %new = prize_params($p, $pr->{id});
         if (!$new{rank} || !$new{name}) {
-            $dbh->do(_u $sql->delete('prizes', { id => $p->{id} }));
+            $dbh->do(_u $sql->delete('prizes', { id => $pr->{id} }));
         }
-        elsif ($new{rank} != $p->{rank} || $new{name} ne $p->{name}) {
-            $dbh->do(_u $sql->update('prizes', \%new, { id => $p->{id} }));
+        elsif ($new{rank} != $pr->{rank} || $new{name} ne $pr->{name}) {
+            $dbh->do(_u $sql->update('prizes', \%new, { id => $pr->{id} }));
         }
     }
-    my %new = prize_params('new');
+    my %new = prize_params($p, 'new');
     if ($new{rank} && $new{name}) {
         $dbh->do(_u $sql->insert('prizes', {
             %new, id => \'gen_id(key_seq, 1)', cg_id => $cgid }));
@@ -64,18 +71,18 @@ sub prizes_edit_save {
 
 sub prizes_frame {
     my ($p) = @_;
-    if ($is_root && (my $cgid = url_param('delete'))) {
+    if ($is_root && $p->{'delete'}) {
         $dbh->do(q~
             DELETE FROM contest_groups WHERE id = ?~, undef,
-            $cgid);
+            $p->{'delete'});
         $dbh->commit;
     }
 
-    $is_root && defined url_param('edit') and return prizes_edit_frame;
+    $is_root && $p->{edit} and return prizes_edit_frame($p);
     init_template($p, 'prizes.html.tt');
     my $lv = CATS::ListView->new(web => $p, name => 'prizes');
 
-    defined param('edit_save') and prizes_edit_save;
+    $p->{edit_save} and prizes_edit_save($p);
 
     $lv->define_columns(url_f('prizes'), 0, 0, [
         { caption => res_str(601), order_by => '2', width => '30%' },
@@ -104,12 +111,13 @@ sub prizes_frame {
     $t->param(submenu => [ CATS::References::menu('prizes') ]);
 }
 
+# TODO
 sub contests_prizes_frame {
     my ($p) = @_;
     init_template($p, 'contests_prizes');
     my $lv = CATS::ListView->new(web => $p, name => 'contests_prizes');
 
-    my @clist = sanitize_clist param('clist');
+    my @clist = sort { $a <=> $b } @{$p->{clist}};
     @clist && @clist < 100 or return;
     my $clist = join ',', @clist;
 
