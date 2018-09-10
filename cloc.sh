4dbf3f37a83ce4d07a0b53d2948db60e09250ade
Alexander Klenin <klenin@gmail.com>
2018-09-10 16:07:20 +1000

    Add cloc script

---
+++ cloc.sh
@@ -0,0 +1,13 @@
+cloc --no3 --force-lang=Perl,t --force-lang=HTML,tt --force-lang=HTML,tti \
+    *.* \
+    cgi-bin/*.* \
+    cgi-bin/CATS \
+    cgi-bin/cats-problem \
+    cgi-bin/t \
+    css \
+    docs/tt \
+    js/*.* \
+    sql/interbase/ \
+    tt/*.* \
+    tt/lang \
+    tt/includes
