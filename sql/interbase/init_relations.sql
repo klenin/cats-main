cef967dd3745c9491a82027ca4353fe157538e62
Alexander Klenin <klenin@gmail.com>
2018-10-06 01:14:42 +1000

    sql: Add contest_wikis table

--- sql/interbase/init_relations.sql
+++ sql/interbase/init_relations.sql
@@ -570,6 +570,19 @@ CREATE TABLE relations (
         FOREIGN KEY (to_id) REFERENCES accounts(id) ON DELETE CASCADE
 );
 
+CREATE TABLE contest_wikis (
+    id         INTEGER NOT NULL PRIMARY KEY,
+    contest_id INTEGER NOT NULL,
+    wiki_id    INTEGER NOT NULL,
+    allow_edit SMALLINT NOT NULL,
+    ordering   SMALLINT NOT NULL,
+
+    CONSTRAINT contest_wikis_contest_fk
+        FOREIGN KEY (contest_id) REFERENCES contests(id) ON DELETE CASCADE,
+    CONSTRAINT contest_wikis_wiki_fk
+        FOREIGN KEY (wiki_id) REFERENCES wiki_pages(id) ON DELETE CASCADE
+);
+
 CREATE GENERATOR key_seq;
 CREATE GENERATOR login_seq;
 
