48ac664c24c6dea34a61b0be5cf68f7c5efa6bd7
Alexander Klenin <klenin@gmail.com>
2019-01-26 19:48:47 +1000

    JudgeDB: Set test_time after selecting request

--- sql/interbase/init_roles.sql
+++ sql/interbase/init_roles.sql
@@ -42,7 +42,7 @@ GRANT UPDATE(status)
 GRANT UPDATE(in_file, in_file_size, in_file_hash, out_file, out_file_size)
     ON TABLE tests TO judge;
 
-GRANT UPDATE(state, failed_test, result_time, judge_id, testsets)
+GRANT UPDATE(state, failed_test, result_time, test_time, judge_id, testsets)
     ON TABLE reqs TO judge;
 
 GRANT DELETE, INSERT
---
+++ sql/interbase/migrations/20190125-test-time.sql
@@ -0,0 +1,2 @@
+GRANT UPDATE(state, failed_test, result_time, test_time, judge_id, testsets)
+    ON TABLE reqs TO judge;
