efe3eb5a351e3c8aea399a5fc0e851c615acd6fc
Alexander Klenin <klenin@gmail.com>
2019-07-22 23:04:43 +1000

    similarity: Add links to view_source

--- cgi-bin/CATS/UI/Stats.pm
+++ cgi-bin/CATS/UI/Stats.pm
@@ -254,6 +254,7 @@ sub similarity_frame {
             $r->{"$_$i"} = $users_idx->{$r->{"t$i"}}->{$_} for qw(name city site);
             $r->{"verdict$i"} = $CATS::Verdicts::state_to_name->{$r->{"req$i"}->{state}};
             $r->{"de_code$i"} = $r->{"req$i"}->{code};
+            $r->{"href_view_source$i"} = url_f('view_source', rid => $r->{"req$i"}->{id});
         }
     }
 
