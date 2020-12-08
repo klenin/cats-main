79082e2c958304b49d17f13e4ba940a0a78d1825
Sergei <hsfk@yandex.ru>
2020-12-08 14:36:04 -0800

    migrate: Support new sql scripts

--- cgi-bin/migrate.pl
+++ cgi-bin/migrate.pl
@@ -18,7 +18,7 @@ Usage: $0
   --help
   --apply=<index>
   --make=<name>
-  --dry_run
+  --dry-run
   --force
 ~;
     exit;
@@ -32,8 +32,9 @@ GetOptions(
     force => \(my $force = 0),
 ) or usage;
 
-my $migration_path = File::Spec->catdir($Bin, qw(.. sql interbase migrations));
--d $migration_path or die "Migrations path not available: $migration_path";
+my $db = $CATS::Config::db;
+printf "DBI: %s\nHost: %s\nDatabase: %s\n",
+    $db->{driver}, $db->{host}, $db->{name};
 
 my $has_lines;
 sub say_c {
@@ -45,27 +46,13 @@ sub say_n {
     say_c @_;
 }
 
-sub make_migration() {
-    $make =~ /^[a-z0-9\-_]+$/ or die "Bad migration name: $make";
-    $make =~ s/_/-/g;
+sub _make_migration {
+    my ($diff, $fout) = @_;
 
-    my $diff = `git diff -- sql/interbase`;
-    $diff or die 'No diff';
-
-    my $name = File::Spec->catfile(
-        $migration_path, POSIX::strftime('%Y%m%d', localtime) . "-$make.sql");
-    say "Creating migration: $name";
-
-    if (-f $name) {
-        my $err = 'Migration already exists';
-        $dry_run ? say $err : $force ? say "$err, forcing" : die $err;
-    }
-
-    if ($dry_run) {
-        say 'Dry run';
-    }
-    else {
-       open STDOUT, '>', $name or die $!;
+    say "Creating migration: $fout";
+    if (!$dry_run) {
+       open my $fh, '>', $fout or die $!;
+       select $fh;
     }
 
     my $table;
@@ -96,9 +83,42 @@ sub make_migration() {
             $table = $1;
         }
     }
+
+    select STDOUT;
 }
 
-sub apply_migration() {
+sub get_migration_path {
+    my $path = File::Spec->catdir($Bin, ('..', 'sql', $_[0], 'migrations'));
+    -d $path or die "Migration path not available: $path";
+    $path;
+}
+
+sub make_migration {
+    $make =~ /^[a-z0-9\-_]+$/ or die "Bad migration name: $make";
+    $make =~ s/_/-/g;
+
+    my $ib_diff = `git diff -- sql/common sql/interbase`;
+    my $pg_diff = `git diff -- sql/common sql/postgres`;
+    $ib_diff || $pg_diff or die 'No diff';
+
+    my $name = POSIX::strftime('%Y%m%d', localtime) . "-$make.sql";
+    my $ib_path = File::Spec->catfile(get_migration_path('interbase'), $name);
+    my $pg_path = File::Spec->catfile(get_migration_path('postgres'), $name);
+
+    if (-f $ib_path) {
+        my $err = 'Migration already exists';
+        $dry_run ? say $err : $force ? say "$err, forcing" : die $err;
+    }
+
+    if ($dry_run) {
+        say 'Dry run';
+    }
+    _make_migration($ib_diff, $ib_path);
+    _make_migration($pg_diff, $pg_path);
+}
+
+sub apply_migration {
+    my $migration_path = get_migration_path($db->{driver} eq 'Firebird' ? 'interbase' : 'postgres');
     opendir my $d, $migration_path or die $!;
     my @files = grep -f, map File::Spec->catfile($migration_path, $_), sort readdir $d;
     my $file = $files[-$apply] or die "No migration for index: $apply";
@@ -108,15 +128,20 @@ sub apply_migration() {
         return;
     }
 
-    my $isql = $^O eq 'Win32' ? 'isql' : 'isql-fb';
-    IPC::Cmd::can_run($isql) or die "Error: $isql not found";
+    my $cmd;
+    if ($db->{driver} eq 'Firebird') {
+        my $isql = $^O eq 'Win32' ? 'isql' : 'isql-fb';
+        IPC::Cmd::can_run($isql) or die "Error: $isql not found";
+        $cmd = [ $isql, '-b', '-i', $file,
+            '-u', $db->{user}, '-p', $db->{password}, '-q', "$db->{host}:$db->{name}" ];
+    } elsif ($db->{driver} eq 'Pg') {
+        IPC::Cmd::can_run('psql') or die 'Error: psql not found';
+        my $dbname = sprintf "postgresql://%s:%s@%s:5432/%s",
+            $db->{user}, $db->{password}, $db->{host}, $db->{name};
+        $cmd = [ 'psql', '-f', $file, '--dbname', $dbname];
+    }
+    $cmd or die "Unknown driver: $db->{driver}";
 
-    my $db = $CATS::Config::db->{name};
-    my $host = $CATS::Config::db->{host};
-    say "Host: $host\nDatabase: $db";
-
-    my $cmd = [ $isql, '-b', '-i', $file,
-        '-u', $CATS::Config::db->{user}, '-p', $CATS::Config::db->{password}, '-q', "$host:$db" ];
     # say join ' ', 'Running:', @$cmd;
     my ($ok, $err, $full) = IPC::Cmd::run command => $cmd;
     $ok or die join "\n", $err, @$full;
