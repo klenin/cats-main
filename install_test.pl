9173744ac3d0437103509cf2c1de8dea9491bfc5
Sergei <hsfk@yandex.ru>
2020-12-14 02:59:47 -0800

    install_test: Use 'trust' authentication type

--- install_test.pl
+++ install_test.pl
@@ -13,7 +13,7 @@ GetOptions(pg => \(my $pg = 0));
 
 if ($pg) {
     CATS::Deploy::create_db('postgres', 'test', 'test_user', 'test_password',
-        init_config => 1, host => 'localhost', quiet => 1);
+        pg_auth_type => 'trust', init_config => 1, host => 'localhost', quiet => 1);
 } else {
     my $tmp_dir = File::Spec->catdir(File::Spec->tmpdir, 'cats');
     CATS::Deploy::create_db('interbase', 'test', 'sysdba', $ENV{ISC_PASSWORD}, dir => $tmp_dir,
