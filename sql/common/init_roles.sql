a3ef539f31937517e07d973bba21948880de8c5b
Alexander Klenin <klenin@gmail.com>
2022-08-02 18:21:37 +1000

    sql/init_roles: Sort tables alphabetically

--- sql/common/init_roles.sql
+++ sql/common/init_roles.sql
@@ -1,51 +1,54 @@
 CREATE ROLE judge;
 
-GRANT SELECT ON TABLE default_de TO judge;
 GRANT SELECT ON TABLE accounts TO judge;
-GRANT SELECT ON TABLE judges TO judge;
-GRANT SELECT ON TABLE judge_de_bitmap_cache TO judge;
-GRANT SELECT ON TABLE contests TO judge;
 GRANT SELECT ON TABLE contest_accounts TO judge;
-GRANT SELECT ON TABLE limits TO judge;
-GRANT SELECT ON TABLE problems TO judge;
-GRANT SELECT ON TABLE problem_de_bitmap_cache TO judge;
 GRANT SELECT ON TABLE contest_problems TO judge;
+GRANT SELECT ON TABLE contests TO judge;
+GRANT SELECT ON TABLE default_de TO judge;
+GRANT SELECT ON TABLE job_sources TO judge;
+GRANT SELECT ON TABLE jobs_queue TO judge;
+GRANT SELECT ON TABLE jobs TO judge;
+GRANT SELECT ON TABLE judge_de_bitmap_cache TO judge;
+GRANT SELECT ON TABLE judges TO judge;
+GRANT SELECT ON TABLE limits TO judge;
+GRANT SELECT ON TABLE logs TO judge;
+GRANT SELECT ON TABLE problem_de_bitmap_cache TO judge;
 GRANT SELECT ON TABLE problem_sources TO judge;
 GRANT SELECT ON TABLE problem_sources_imported TO judge;
 GRANT SELECT ON TABLE problem_sources_local TO judge;
+GRANT SELECT ON TABLE problems TO judge;
+GRANT SELECT ON TABLE req_de_bitmap_cache TO judge;
+GRANT SELECT ON TABLE req_details TO judge;
+GRANT SELECT ON TABLE req_groups TO judge;
+GRANT SELECT ON TABLE reqs TO judge;
+GRANT SELECT ON TABLE solution_output TO judge;
+GRANT SELECT ON TABLE sources TO judge;
 GRANT SELECT ON TABLE tests TO judge;
 GRANT SELECT ON TABLE testsets TO judge;
-GRANT SELECT ON TABLE reqs TO judge;
-GRANT SELECT ON TABLE req_de_bitmap_cache TO judge;
-GRANT SELECT ON TABLE req_groups TO judge;
-GRANT SELECT ON TABLE req_details TO judge;
-GRANT SELECT ON TABLE solution_output TO judge;
-GRANT SELECT ON TABLE logs TO judge;
-GRANT SELECT ON TABLE sources TO judge;
-GRANT SELECT ON TABLE jobs TO judge;
-GRANT SELECT ON TABLE jobs_queue TO judge;
-GRANT SELECT ON TABLE job_sources TO judge;
 
 GRANT UPDATE(sid, last_login, last_ip)
     ON TABLE accounts TO judge;
 
-GRANT UPDATE(version, is_alive, alive_date)
-    ON TABLE judges TO judge;
+GRANT UPDATE(status)
+    ON TABLE contest_problems TO judge;
+
+GRANT DELETE, INSERT
+    ON TABLE jobs_queue TO judge;
+
+GRANT UPDATE(state, start_time, finish_time, judge_id)
+    ON TABLE jobs TO judge;
 
 GRANT INSERT, UPDATE
     ON TABLE judge_de_bitmap_cache TO judge;
 
+GRANT UPDATE(version, is_alive, alive_date)
+    ON TABLE judges TO judge;
+
+GRANT INSERT
+    ON TABLE logs TO judge;
+
 GRANT DELETE, INSERT, UPDATE
     ON TABLE problem_de_bitmap_cache TO judge;
-
-GRANT UPDATE(status)
-    ON TABLE contest_problems TO judge;
-
-GRANT UPDATE(in_file, in_file_size, in_file_hash, out_file, out_file_size)
-    ON TABLE tests TO judge;
-
-GRANT UPDATE(state, failed_test, result_time, test_time, judge_id, testsets)
-    ON TABLE reqs TO judge;
 
 GRANT DELETE, INSERT
     ON TABLE req_de_bitmap_cache TO judge;
@@ -53,14 +56,11 @@ GRANT DELETE, INSERT
 GRANT DELETE, INSERT
     ON TABLE req_details TO judge;
 
+GRANT UPDATE(state, failed_test, result_time, test_time, judge_id, testsets)
+    ON TABLE reqs TO judge;
+
 GRANT INSERT
     ON TABLE solution_output TO judge;
 
-GRANT INSERT
-    ON TABLE logs TO judge;
-
-GRANT UPDATE(state, start_time, finish_time, judge_id)
-    ON TABLE jobs TO judge;
-
-GRANT DELETE, INSERT
-    ON TABLE jobs_queue TO judge;
+GRANT UPDATE(in_file, in_file_size, in_file_hash, out_file, out_file_size)
+    ON TABLE tests TO judge;
