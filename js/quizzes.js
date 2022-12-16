1da7385a3a4780a22a9d7b6a18eb250764d87023
Alexander Klenin <klenin@gmail.com>
2022-12-16 15:28:51 +1000

    quizzes: Stop using SurveyJS

--- js/quizzes.js
+++ js/quizzes.js
@@ -1,57 +1,63 @@
 "use strict";
 
 function init_quizzes() {
-  Survey.StylesManager.applyTheme('default');
   $('.problem_text').each(function() {
-    var survey_models = [];
+    var cpid = this.id.substr(1);
     var problem_text = $(this);
+    var questions = problem_text.data('quiz');
+    if (!questions) {
+      questions = [];
+      problem_text.data('quiz', questions)
+    }
     var editor = Editor.get_editor(problem_text);
     var quiz_count = 0;
     var editor_changed_callback = fill_quiz_forms.bind(null, editor, problem_text);
     problem_text.find('Quiz').each(function() {
       var quiz = $(this);
-      if (quiz.find('form').length) return;
+      if (quiz.hasClass('active')) return;
+      quiz.addClass('active');
       quiz.find('Text').each(function() {
-        $(this).replaceWith($('<p></p>', {html: $(this).html()}));
+        $(this).replaceWith($('<p></p>', { html: $(this).html() }));
       });
-      var form = $('<form/>');
-      quiz.addClass('active').append(form);
-      form.attr('onsubmit', 'return false;');
+      quiz_count++;
       var question = { 
         type: quiz.attr('type'), 
         name: 'Quiz' + quiz_count,
-        titleLocation: 'hidden'
       };
-      if (question.type === 'radiogroup' || question.type === 'checkbox')
+      var changed_callback = function() {
+        var quiz_number = quiz_count;
+        return function () { quiz_value_changed(editor, this, quiz_number, editor_changed_callback); };
+      }();
+
+      if (question.type === 'radiogroup' || question.type === 'checkbox') {
         question.choices = $.map(quiz.children('Choice').remove(), function(choice, i) {
           return { value: i + 1, text: choice.innerHTML };
         });
-      var survey = new Survey.Model({
-        questionTitleTemplate: '{no}',
-        questions: [ question ],
-        showNavigationButtons: false,
-        showQuestionNumbers: 'off'
-      });
-      survey.onTextMarkdown.add(function(survey, options) {
-        options.html = options.text;
-      });
-      var survey_changed_callback = function() {
-        var quiz_number = quiz_count;
-        return function (survey, options) { survey_value_changed(editor, quiz_number, survey, editor_changed_callback); };
-      }();
-      survey.onValueChanged.add(survey_changed_callback);
-      form.Survey({ model: survey });
-      form.append('<input type="submit" hidden="true">');
-      if (question.type === 'text')
-        quiz.find('input').attr('pattern', '\\S+');
-      survey.changed_callback = survey_changed_callback;
-      survey_models.push(survey);
-      quiz_count++;
+      }
+      var make_choices = function (type) {
+        var choices_div = $('<div>').appendTo(quiz);
+        for (var i = 0; i < question.choices.length; ++i) {
+          var para = $('<p>').appendTo(choices_div);
+          var label = $('<label>').html(question.choices[i].text).appendTo(para);
+          var inp = $('<input type="' + type + '" name="p' + cpid + '_q' + quiz_count + '" value="' + question.choices[i].value + '"/>').
+            prependTo(label).change(changed_callback);
+        }
+      };
+      if (question.type === 'radiogroup') {
+        make_choices('radio');
+      }
+      else if (question.type === 'checkbox') {
+        make_choices('checkbox');
+      }
+      else if (question.type === 'text') {
+        var inp = $('<input type="text" name="p' + cpid + '_q' + quiz_count + '" class="bordered"/>').attr('pattern', '\\S+').
+          appendTo(quiz).change(changed_callback);
+      }
+      questions.push(question);
     });
     if (quiz_count) {
       problem_text.find('.show_editor').click(); // Toggle editor to hidden.
       problem_text.find('.editor_only').hide();
-      problem_text.data('survey_models', survey_models);
       editor.getSession().addEventListener('change', editor_changed_callback);
       fill_quiz_forms(editor, problem_text);
       setTimeout(function() {
@@ -62,43 +68,62 @@ function init_quizzes() {
 }
 
 function fill_quiz_forms(editor, problem_text) {
-  var survey_models = problem_text.data('survey_models');
+  var questions = problem_text.data('quiz');
+  var cpid = problem_text[0].id.substr(1);
   var answers = editor.getValue().split(/\r?\n/);
-  for (var i = 0; i < survey_models.length; ++i) {
-    var sm = survey_models[i];
-    sm.onValueChanged.remove(sm.changed_callback);
-    var question = sm.getQuestionByName('Quiz' + i);
-    if (question.getType() === 'checkbox' && answers[i] !== undefined)
-      question.value = (answers[i] === '') ? [] : answers[i].split(' ');
-    else
-      question.value = answers[i];
-    sm.onValueChanged.add(sm.changed_callback);
+  for (var i = 0; i < questions.length; ++i) {
+    var question = questions[i];
+    var inp = problem_text.find('input[name=p' + cpid + '_q' + (i + 1) + ']');
+    if (question.type === 'radiogroup') {
+      inp.each(function () {
+        this.checked = this.value === answers[i];
+      });
+    }
+    else if (question.type === 'checkbox') {
+      var answer_items = {};
+      if (answers[i])
+        answers[i].split(' ').forEach(function (a) { answer_items[a] = 1; });
+      inp.each(function () {
+        this.checked = answer_items[this.value];
+      });
+    }
+    else if (question.type === 'text') {
+      inp.val(answers[i]);
+    }
   }
 }
-  
-function survey_value_changed(editor, question_number, survey, editor_changed_callback) {
-  var question = survey.getQuestionByName('Quiz' + question_number);
+
+function quiz_value_changed(editor, question, question_number, editor_changed_callback) {
   editor.getSession().removeEventListener('change', editor_changed_callback);
   var answers = editor.getValue().split('\n');
-  if (answers.length > question_number) {
-    editor.moveCursorTo(question_number);
+  if (answers.length >= question_number) {
+    editor.moveCursorTo(question_number - 1);
     editor.navigateLineEnd();
     editor.removeToLineStart();
   }
   else {
     editor.moveCursorTo(answers.length);
-    for (var i = answers.length; i <= question_number; ++i)
+    for (var i = answers.length; i < question_number; ++i)
       editor.insert('\n');
   }
+
+  var q_elem = $(question);
   // TODO: Add UI for unanswering question.
-  if (question.value === undefined) return;
-  if (question.getType() === 'checkbox')
-    editor.insert(question.value.join(' '));
-  else
-    editor.insert(question.value.toString());
+  var value;
+  if (q_elem.attr('type') === "radio")
+    value = q_elem.val()
+  else if (q_elem.attr('type') === "checkbox") {
+    value = q_elem.parent().parent().parent().find('input:checked').
+      map(function (_, el) { return el.value; }).toArray().join(' ');
+  }
+  else if (q_elem.attr('type') === "checkbox") {
+    value = q_elem.val()
+  }
+  if (value !== undefined)
+    editor.insert(value);
   editor.getSession().addEventListener('change', editor_changed_callback);
 }
-  
+
 function is_quiz_input_valid(quiz, msg) {
   var input = quiz.find('input[type="text"]')[0];
   if (!input) return true;
@@ -108,7 +133,7 @@ function is_quiz_input_valid(quiz, msg) {
   quiz.find('input[type="submit"]').click();
   return false;
 }
-  
+
 function is_all_quiz_input_valid(form, msg) {
   var is_valid = true;
   var problem_text = form.parents('.problem_text');
