5c1daad4248ab042f9a7decc4fb22200e02d865a
Alexander Klenin <klenin@gmail.com>
2023-01-12 20:01:03 +1000

    health_report: Extract report items

--- cgi-bin/health_report.pl
+++ cgi-bin/health_report.pl
@@ -63,84 +63,107 @@ if (!$output || $output !~ /^(std|mail)$/) {
     usage;
 }
 
-my $r = CATS::Report->new;
-
 CATS::DB::sql_connect({
     ib_timestampformat => '%d.%m.%Y %H:%M',
     ib_dateformat => '%d.%m.%Y',
     ib_timeformat => '%H:%M:%S',
 });
 
-{
-    $r->{long}->{'Queue length'} = my $ql = $dbh->selectrow_array(qq~
-        SELECT COUNT(*) FROM reqs R
-            WHERE R.state = $cats::st_not_processed AND R.submit_time > CURRENT_TIMESTAMP - 30~);
-    $r->{short}->{Q} = $ql if $ql > 1;
-}
-
-{
-    $r->{long}->{'Job queue'} = my $jql = $dbh->selectrow_array(q~
-        SELECT COUNT(*) FROM jobs_queue~);
-    $r->{short}->{JQ} = $jql if $jql > 1;
-}
-
-{
-    $r->{long}->{'Requests today'} = $dbh->selectrow_array(q~
-        SELECT COUNT(*) FROM reqs R
-            WHERE R.submit_time > CURRENT_TIMESTAMP - 1~);
-}
-
-{
-    $r->{long}->{'Jobs today'} = $dbh->selectrow_array(q~
-        SELECT COUNT(*) FROM jobs J
-            WHERE J.create_time > CURRENT_TIMESTAMP - 1~);
-}
-
 sub log_url {
     CATS::Utils::absolute_url_function(
         'run_log', rid => $_[0]->{id}, cid => $_[0]->{contest_id}, sid => 'z')
 }
 
-{
-    my $u = $dbh->selectall_arrayref(qq~
-        SELECT R.id, R.contest_id FROM reqs R
-            WHERE R.state = $cats::st_unhandled_error AND R.submit_time > CURRENT_TIMESTAMP - 30
-            ORDER BY R.submit_time DESC~, { Slice => {} });
-    $r->{long}->{'Unhandled errors'} = join "\n", scalar(@$u), map '    ' . log_url($_), @$u;
-    $r->{short}->{U} = scalar @$u if @$u;
-}
+my $report_items = [
+    {
+        name => 'ReqQ', long => 'Queue length', short => 'Q',
+        get => sub {
+            my $ql = $dbh->selectrow_array(qq~
+                SELECT COUNT(*) FROM reqs R
+                WHERE R.state = ? AND R.submit_time > CURRENT_TIMESTAMP - 30~, undef,
+                $cats::st_not_processed);
+            ($ql, ($ql > 1 ? $ql : undef));
+        },
+    },
+    {
+        name => 'JobQ', long => 'Job queue', short => 'JQ',
+        get => sub {
+            my $jql = $dbh->selectrow_array(q~
+                SELECT COUNT(*) FROM jobs_queue~);
+            ($jql, ($jql > 1 ? $jql : undef));
+        },
+    },
+    {
+        name => 'ReqT', long => 'Requests today', short => '',
+        get => sub {
+            scalar $dbh->selectrow_array(q~
+                SELECT COUNT(*) FROM reqs R
+                WHERE R.submit_time > CURRENT_TIMESTAMP - 1~);
+        },
+    },
+    {
+        name => 'JobT', long => 'Jobs today', short => '',
+        get => sub {
+            scalar $dbh->selectrow_array(q~
+                SELECT COUNT(*) FROM jobs J
+                WHERE J.create_time > CURRENT_TIMESTAMP - 1~);
+        },
+    },
+    {
+        name => 'UH', long => 'Unhandled errors', short => 'UH',
+        get => sub {
+            my $u = $dbh->selectall_arrayref(qq~
+                SELECT R.id, R.contest_id FROM reqs R
+                WHERE R.state = $cats::st_unhandled_error AND R.submit_time > CURRENT_TIMESTAMP - 30
+                ORDER BY R.submit_time DESC~, { Slice => {} });
+            ((join "\n", scalar(@$u), map '    ' . log_url($_), @$u), (@$u ? scalar @$u : undef));
+        },
+    },
+    {
+        name => 'Judge', long => 'Judges alive / total', short => 'J',
+        get => sub {
+            my ($jalive, $jtotal) = CATS::Judge::get_active_count;
+            my $j = "$jalive/$jtotal";
+            ($j, ($jalive < $jtotal || !$jtotal ? $j : undef));
+        },
+    },
+    {
+        name => 'Quest', long => 'Questions unanswered', short => '?',
+        get => sub {
+            my $q = $dbh->selectrow_array(q~
+                SELECT COUNT(*) FROM questions Q
+                WHERE Q.clarified = 0 AND Q.submit_time > CURRENT_TIMESTAMP - 30~);
+            ($q, $q || undef);
+        },
+    },
+    {
+        name => 'Prob', long => 'Problems changed', short => '',
+        get => sub {
+            my ($p) = $dbh->selectall_arrayref(q~
+                SELECT id, title
+                FROM problems P WHERE CURRENT_TIMESTAMP - P.upload_date <= 1~, { Slice => {} });
+            (@$p ? join ', ', map qq~"$_->{title}"~, @$p : undef);
+        },
+    },
+    {
+        name => 'Log', long => 'Log dump size', short => '',
+        get => sub {
+            my ($length) = $dbh->selectrow_array(q~
+                SELECT SUM(OCTET_LENGTH(L.dump))
+                FROM logs L INNER JOIN jobs J ON J.id = L.job_id
+                WHERE J.create_time > CURRENT_TIMESTAMP - 1~);
+            ($length ? group_digits($length, '_') : undef);
+        },
+    },
+];
 
-{
-    my ($jalive, $jtotal) = CATS::Judge::get_active_count;
-    $r->{long}->{'Judges total'} = $jtotal;
-    $r->{long}->{'Judges alive'} = $jalive;
-    $r->{short}->{J} = "$jalive/$jtotal" if $jalive < $jtotal || !$jtotal;
-}
+my $r = CATS::Report->new;
 
-{
-    $r->{long}->{'Questions unanswered'} = my $q = $dbh->selectrow_array(q~
-        SELECT COUNT(*) FROM questions Q
-            WHERE Q.clarified = 0 AND Q.submit_time > CURRENT_TIMESTAMP - 30~);
-    $r->{short}->{'?'} = $q if $q;
-}
-
-{
-    my ($p) = $dbh->selectall_arrayref(q~
-        SELECT id, title
-            FROM problems P WHERE CURRENT_TIMESTAMP - P.upload_date <= 1~, { Slice => {} });
-    if (@$p) {
-        $r->{long}->{'Problems changed'} = join ', ', map qq~"$_->{title}"~, @$p;
-    }
-}
-
-{
-    my ($length) = $dbh->selectrow_array(q~
-        SELECT SUM(OCTET_LENGTH(L.dump))
-        FROM logs L INNER JOIN jobs J ON J.id = L.job_id
-        WHERE J.create_time > CURRENT_TIMESTAMP - 1~);
-    if ($length) {
-        $r->{long}->{'Log dump size'} = group_digits($length, '_');
-    }
+for my $ri (@$report_items) {
+    print "$ri->{long}...\n" if $verbose;
+    my ($long, $short) = $ri->{get}->();
+    $r->{long}->{$ri->{long}} = $long if defined $long;
+    $r->{short}->{$ri->{short}} = $short if defined $short;
 }
 
 $dbh->disconnect;
