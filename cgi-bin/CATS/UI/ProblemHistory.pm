1be7d131da31fa98909cf9d87086b2dccadc1dcf
Alexander Klenin <klenin@gmail.com>
2023-02-19 02:55:43 +1000

    problem_history_edit: Linkify guids

--- cgi-bin/CATS/UI/ProblemHistory.pm
+++ cgi-bin/CATS/UI/ProblemHistory.pm
@@ -282,6 +282,7 @@ sub problem_history_edit_frame {
         image_extension_re => substr($CATS::DetectImage::image_extension_re, 4, -1), # Strip (?^:...).
         href_blob_prefix => url_f('problem_history_blob', pid => $p->{pid}, hb => $hash_base),
         href_edit_prefix => url_f('problem_history_edit', pid => $p->{pid}, hb => $hash_base),
+        href_import_source_prefix => url_f('import_source_view'),
     );
 }
 
