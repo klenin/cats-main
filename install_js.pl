4e7836d74282a44d4dee56021f93d216640626db
Alexander Klenin <klenin@gmail.com>
2023-03-28 11:32:51 +1000

    install_js: Indicate installed modules

--- install_js.pl
+++ install_js.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 
 use File::Path 'remove_tree';
+use File::Spec;
 use Getopt::Long;
 
 GetOptions(
@@ -82,9 +83,20 @@ my $modules = [
     },
 ];
 
+sub prepare_names {
+    my ($m) = @_;
+    my ($filename) = ($m->{url} =~ /\/([a-zA-Z0-9_\-\.]+)$/) or die "Bad url: $m->{url}";
+    my $src = $m->{src} || $filename;
+    my $dest = $m->{dest} // '';
+    ($filename, $src, $dest);
+}
+
 if ($list) {
     for my $m (@$modules) {
-        printf "%12s: %s\n", $m->{name}, $m->{url};
+        my ($filename, $src, $dest) = prepare_names($m);
+        my (undef, undef, $src_file) = File::Spec->splitpath($src);
+        my $installed = -e File::Spec->catfile($path, $dest || $src_file);
+        printf "%12s: %s %s\n", $m->{name}, ($installed ? '+' : ' '), $m->{url};
     }
 }
 
@@ -113,7 +125,7 @@ if (@$install) {
         printf "Installing: %s...%s", $name, $dbg{verbose} ? "\n" : '';
         my $m = $modules_index{$name};
         my $q = $dbg{verbose} ? '' : '-q';
-        my ($filename) = ($m->{url} =~ /\/([a-zA-Z0-9_\-\.]+)$/) or die $m->{url};
+        my ($filename, $src, $dest) = prepare_names($m);
         if (-e $filename) {
             printf "Already exists: %s\n", $filename if $dbg{verbose};
         }
@@ -123,8 +135,6 @@ if (@$install) {
         if ($filename =~ /\.zip$/) {
             `unzip -o $q $filename`;
         }
-        my $src = $m->{src} || $filename;
-        my $dest = $m->{dest} // '';
         printf "==> %s\n", $filename if $dbg{verbose};
         `mv $src ../$path/$dest`;
         print "ok\n";
