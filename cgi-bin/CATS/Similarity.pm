0384a19c6161aacecf96a5e6fc80ef96f0874335
Alexander Klenin <klenin@gmail.com>
2023-11-21 06:47:35 +1000

    Similarity: Add recommend_replacements

--- cgi-bin/CATS/Similarity.pm
+++ cgi-bin/CATS/Similarity.pm
@@ -6,13 +6,15 @@ use warnings;
 use Encode;
 use Digest::MD5;
 
+my $collapded_id = '_Q_';
+
 sub preprocess_line {
     my ($s) = @_;
     use bytes; # MD5 works with bytes, prevent upgrade to utf8
     s/\s+//g;
     #s/#.+$//; s[//.+$][];
     if ($s->{collapse_idents}) {
-        s/[a-zA-Z_][a-zA-Z_0-9]*/A/g;
+        s/[a-zA-Z_][a-zA-Z_0-9]*/$collapded_id/g;
     }
     else {
         $_ = uc($_);
@@ -34,7 +36,7 @@ sub preprocess_source {
         s/\s+//g;
         #s/#.+$//; s[//.+$][];
         if ($s->{collapse_idents}) {
-            s/[a-zA-Z_][a-zA-Z_0-9]*/A/g;
+            s/[a-zA-Z_][a-zA-Z_0-9]*/$collapded_id/g;
         }
         else {
             $_ = uc($_);
@@ -58,4 +60,50 @@ sub similarity_score_2 {
     similarity_score(map $_->{hash}, $req1, $req2);
 }
 
+sub recommend_replacements {
+    my ($req1, $req2, $s, $except) = @_;
+
+
+    my $max_lines = $s->{max_lines} || 1000;
+    my $line_num_1 = 0;
+    my %renames;
+
+    for my $line1 (split /\n/, $req1->{src}) {
+        ++$line_num_1 < $max_lines or last;
+        my @list1 = split /([a-zA-Z_][a-zA-Z_0-9]*)/, $line1;
+
+        my $line_num_2 = 0;
+        for my $line2 (split /\n/, $req2->{src}) {
+            ++$line_num_2 < $max_lines or last;
+            next if $line1 eq $line2;
+            my @list2 = split /([a-zA-Z_][a-zA-Z_0-9]*)/, $line2;
+            next if @list1 != @list2 || @list1 <= 1;
+
+            my $is_similar = 1;
+            my (%line_renames_count, %line_renames);
+            for (my $i = 0; $i < @list1; $i += 2) {
+                $list1[$i] =~ s/\s+//g;
+                $list2[$i] =~ s/\s+//g;
+                $is_similar = $list1[$i] eq $list2[$i] or last;
+
+                $i + 1 < @list1 or last;
+                my ($id1, $id2) = ($list1[$i + 1], $list2[$i + 1]);
+
+                $line_renames{$id1} //= $id2;
+                $is_similar = $line_renames{$id1} eq $id2 or last;
+
+                next if $id1 eq $id2 || $except->{$id1} || $except->{$id2};
+
+                ++$line_renames_count{"$id1 $id2"};
+            }
+
+            if ($is_similar) {
+                $renames{$_} += $line_renames_count{$_} for keys %line_renames_count;
+            }
+        }
+    }
+    my @result =
+        sort { $renames{$b} <=> $renames{$a} || $a cmp $b } grep $renames{$_} > 1, keys %renames;
+    (\@result, [ map $renames{$_}, @result ]);
+}
 1;
--- cgi-bin/t/similarity.t
+++ cgi-bin/t/similarity.t
@@ -3,7 +3,7 @@ use warnings;
 
 use File::Spec;
 use FindBin;
-use Test::More tests => 2;
+use Test::More tests => 10;
 
 use lib File::Spec->catdir($FindBin::Bin);
 use lib File::Spec->catdir($FindBin::Bin, '..');
@@ -27,4 +27,43 @@ sub sc { CATS::Similarity::similarity_score(map $_->{hash}, @_) }
     is sc($req1, $req2), 1.0, 'collapse idents';
 }
 
+{
+    my $req1 = { src => "print(a)\n    a = 0" };
+    my $req2 = { src => "print( b )\nb = 0" };
+    my ($r, $c) = CATS::Similarity::recommend_replacements($req1, $req2, {}, {});
+    is_deeply $r, [ 'a b' ], 'recommend simple str';
+    is_deeply $c, [ 2 ], 'recommend simple cnt';
+}
+
+{
+    my $req1 = { src => "a a\nc\na a\n!c\n" };
+    my $req2 = { src => "b a\nd\nb a\n!d\n" };
+    my ($r, $c) = CATS::Similarity::recommend_replacements($req1, $req2, {}, {});
+    is_deeply $r, [ 'c d' ], 'recommend consistent line str';
+    is_deeply $c, [ 2 ], 'recommend consistent line cnt';
+}
+
+{
+    my $req1 = { src => "a\nc\na+\nc c\n" };
+    my $req2 = { src => "b\nd\nb+\nd d\n" };
+    my ($r, $c) = CATS::Similarity::recommend_replacements($req1, $req2, {}, {});
+    is_deeply $r, [ 'c d', 'a b' ], 'recommend order str';
+    is_deeply $c, [ 3, 2 ], 'recommend order cnt';
+}
+
+{
+    my $req1 = { src => "a\nc\na+\nc c\n" };
+    my $req2 = { src => "b\nd\nb+\nd d\n" };
+    my ($r, $c) = CATS::Similarity::recommend_replacements($req1, $req2, {}, { c => 1 });
+    is_deeply $r, [ 'a b' ], 'recommend except str';
+    is_deeply $c, [ 2 ], 'recommend except cnt';
+}
+
+{
+    my $req1 = { src => "a = int(input())\nfor i in range(a):\n" };
+    my $req2 = { src => "kolich = int(input())\nfor i in range(kolich):\n" };
+    my ($r, $c) = CATS::Similarity::recommend_replacements($req1, $req2, {}, { c => 1 });
+    is_deeply $r, [ 'a kolich' ], 'recommend except str';
+    is_deeply $c, [ 2 ], 'recommend except cnt';
+}
 1;
